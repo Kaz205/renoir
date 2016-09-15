14d9daec2b8e592166c46b1dcb4bbac88530bec3
John Dias <joaodias@google.com>
2016-09-15 08:52:27 -0700

    ANDROID: sched: avoid placing RT threads on cores handling softirqs
    In certain audio use cases, scheduling RT threads on cores that are
    handling softirqs can lead to glitches. Prevent this behavior.
    
    Bug: 31501544
    Bug: 168521633
    Change-Id: I99dd7aaa12c11270b28dbabea484bcc8fb8ba0c1
    Signed-off-by: John Dias <joaodias@google.com>
    [elavila: Port to mainline, amend commit text]
    Signed-off-by: J. Avila <elavila@google.com>
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- kernel/sched/cpupri.c
+++ kernel/sched/cpupri.c
@@ -41,8 +41,27 @@ static int convert_prio(int prio)
 	return cpupri;
 }
 
+/**
+ * drop_nopreempt_cpus - remove likely nonpreemptible cpus from the mask
+ * @lowest_mask: mask with selected CPUs (non-NULL)
+ */
+static void
+drop_nopreempt_cpus(struct cpumask *lowest_mask)
+{
+	unsigned cpu = cpumask_first(lowest_mask);
+	while (cpu < nr_cpu_ids) {
+		/* unlocked access */
+		struct task_struct *task = READ_ONCE(cpu_rq(cpu)->curr);
+		if (task_may_not_preempt(task, cpu)) {
+			cpumask_clear_cpu(cpu, lowest_mask);
+		}
+		cpu = cpumask_next(cpu, lowest_mask);
+	}
+}
+
 static inline int __cpupri_find(struct cpupri *cp, struct task_struct *p,
-				struct cpumask *lowest_mask, int idx)
+				struct cpumask *lowest_mask, int idx,
+				bool drop_nopreempts)
 {
 	struct cpupri_vec *vec  = &cp->pri_to_cpu[idx];
 	int skip = 0;
@@ -78,6 +97,9 @@ static inline int __cpupri_find(struct cpupri *cp, struct task_struct *p,
 
 	if (lowest_mask) {
 		cpumask_and(lowest_mask, p->cpus_ptr, vec->mask);
+
+		if (drop_nopreempts)
+			drop_nopreempt_cpus(lowest_mask);
 
 		/*
 		 * We have to ensure that we have at least one bit
@@ -123,12 +145,14 @@ int cpupri_find_fitness(struct cpupri *cp, struct task_struct *p,
 {
 	int task_pri = convert_prio(p->prio);
 	int idx, cpu;
+	bool drop_nopreempts = task_pri <= MAX_RT_PRIO;
 
 	BUG_ON(task_pri >= CPUPRI_NR_PRIORITIES);
 
+retry:
 	for (idx = 0; idx < task_pri; idx++) {
 
-		if (!__cpupri_find(cp, p, lowest_mask, idx))
+		if (!__cpupri_find(cp, p, lowest_mask, idx, drop_nopreempts))
 			continue;
 
 		if (!lowest_mask || !fitness_fn)
@@ -148,6 +172,15 @@ int cpupri_find_fitness(struct cpupri *cp, struct task_struct *p,
 			continue;
 
 		return 1;
+	}
+
+	/*
+	 * If we can't find any non-preemptible cpu's, retry so we can
+	 * find the lowest priority target and avoid priority inversion.
+	 */
+	if (drop_nopreempts) {
+		drop_nopreempts = false;
+		goto retry;
 	}
 
 	/*
