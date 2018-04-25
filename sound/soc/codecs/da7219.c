caa61999fb4d808e5bf1710c4302e2b92f4c0d8b
Akshu Agrawal <akshu.agrawal@amd.com>
2018-04-25 16:52:34 +0800

    FROMLIST: ASoC: da7219: read fmw property to get mclk for non-dts systems
    Relands
    
    commit cab0735c0fa8b ("FROMLIST: ASoC: da7219: read fmw property to get mclk for non-dts systems")
    
    After the component change in codec driver.
    
    Non-dts based systems can use ACPI DSDT to pass on the mclk
    to da7219.
    This enables da7219 mclk to be linked to system clock.
    Enable/Disable of the mclk is already handled in the codec so
    platform drivers don't have to explicitly do handling of mclk.
    
    Signed-off-by: Akshu Agrawal <akshu.agrawal@amd.com>
    
    (am from https://patchwork.kernel.org/patch/10377275/)
    
    BUG=b:80592327, b:74570989
    TEST=aplay -vv <file>
      check register to see clock enabled
      kill aplay
      check register to see clock disabled
    
    Reviewed-on: https://chromium-review.googlesource.com/1027735
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Signed-off-by: Cheng-Yi Chiang <cychiang@chromium.org>
    
    Change-Id: Ic8c86cfb53da8c62644f4aef89f8777853082dad
    Reviewed-on: https://chromium-review.googlesource.com/1139727
    Commit-Ready: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- sound/soc/codecs/da7219.c
+++ sound/soc/codecs/da7219.c
@@ -1773,6 +1773,8 @@ static struct da7219_pdata *da7219_fw_to_pdata(struct snd_soc_component *compone
 			 pdata->dai_clk_names[DA7219_DAI_WCLK_IDX],
 			 pdata->dai_clk_names[DA7219_DAI_BCLK_IDX]);
 
+	device_property_read_string(dev, "dlg,mclk-name", &pdata->mclk_name);
+
 	if (device_property_read_u32(dev, "dlg,micbias-lvl", &of_val32) >= 0)
 		pdata->micbias_lvl = da7219_fw_micbias_lvl(dev, of_val32);
 	else
@@ -2296,7 +2298,11 @@ static int da7219_probe(struct snd_soc_component *component)
 	da7219_handle_pdata(component);
 
 	/* Check if MCLK provided */
-	da7219->mclk = devm_clk_get(component->dev, "mclk");
+	if (da7219->pdata->mclk_name)
+		da7219->mclk = clk_get(NULL, da7219->pdata->mclk_name);
+	if (!da7219->mclk)
+		da7219->mclk = devm_clk_get(component->dev, "mclk");
+
 	if (IS_ERR(da7219->mclk)) {
 		if (PTR_ERR(da7219->mclk) != -ENOENT) {
 			ret = PTR_ERR(da7219->mclk);
@@ -2371,6 +2377,9 @@ static void da7219_remove(struct snd_soc_component *component)
 			clkdev_drop(da7219->dai_clks_lookup[i]);
 	}
 #endif
+
+	if (da7219->pdata->mclk_name)
+		clk_put(da7219->mclk);
 
 	/* Supplies */
 	regulator_bulk_disable(DA7219_NUM_SUPPLIES, da7219->supplies);
