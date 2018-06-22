32e93e1f1c560ff32f48163cbaa2e1e239e1d965
Will Deacon <will.deacon@arm.com>
2018-06-22 09:31:16 +0100

    arm64: Remove unnecessary ISBs from set_{pte,pmd,pud}
    Commit 7f0b1bf04511 ("arm64: Fix barriers used for page table modifications")
    fixed a reported issue with fixmap page-table entries not being visible
    to the walker due to a missing DSB instruction. At the same time, it added
    ISB instructions to the arm64 set_{pte,pmd,pud} functions, which are not
    required by the architecture and make little sense in isolation.
    
    Remove the redundant ISBs.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Change-Id: I4dab0d6a37efddded320702e54b7ba09cd3d8a7c

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -230,10 +230,8 @@ static inline void set_pte(pte_t *ptep, pte_t pte)
 	 * Only if the new pte is valid and kernel, otherwise TLB maintenance
 	 * or update_mmu_cache() have the necessary barriers.
 	 */
-	if (pte_valid_not_user(pte)) {
+	if (pte_valid_not_user(pte))
 		dsb(ishst);
-		isb();
-	}
 }
 
 extern void __sync_icache_dcache(pte_t pteval);
@@ -496,7 +494,6 @@ static inline void set_pmd(pmd_t *pmdp, pmd_t pmd)
 
 	if (pmd_valid(pmd)) {
 		dsb(ishst);
-		isb();
 	}
 }
 
@@ -562,7 +559,6 @@ static inline void set_pud(pud_t *pudp, pud_t pud)
 
 	if (pud_valid(pud)) {
 		dsb(ishst);
-		isb();
 	}
 }
 
