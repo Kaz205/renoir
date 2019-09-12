2507b5ab6110fcd2355cbbffa4e14613dcf7463a
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-09-12 13:41:01 +0900

    UPSTREAM: ASoC: soc-core: merge soc_free_pcm_runtime() and soc_rtd_free()
    "rtd"      is handled by soc_xxx_pcm_runtime(), and
    "rtd->dev" is handled by soc_rtd_xxx().
    
    There is no reason to separate these, and it makes code complex.
    We can free these in the same time.
    
    Here soc_rtd_free() (A) which frees rtd->dev is called from
    soc_remove_link_dais() many times (1).
    Then, it is using dev_registered flags to avoid multi kfree() (2).
    This is no longer needed if we can merge these functions.
    
    	static void soc_remove_link_dais(...)
    	{
    		...
    (1)		for_each_comp_order(order) {
    (1)			for_each_card_rtds(card, rtd) {
    
    (A)				soc_rtd_free(rtd);
    				...
    			}
    		}
    	}
    
    (A)	static void soc_rtd_free(...)
    	{
    (2)		if (rtd->dev_registered) {
    			/* we don't need to call kfree() for rtd->dev */
    			device_unregister(rtd->dev);
    (2)			rtd->dev_registered = 0;
    		}
    	}
    
    This patch merges soc_rtd_free() into soc_free_pcm_runtime().
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/878squf7oi.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit b7c5bc45ee94a03a0dc45a862180e17db8ea8e9d)
    Signed-off-by: Chintan Patel <chintan.m.patel@intel.com>
    
    BUG=b:147436144
    TEST=Audio playback via Speaker, Headphone and HDMI/DP
    
    Change-Id: I950bbc355145ba9b1c0bd26a0b09a2e23abc3a9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2029577
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Tested-by: Curtis Malainey <cujomalainey@chromium.org>
    Commit-Queue: Curtis Malainey <cujomalainey@chromium.org>

--- include/sound/soc.h
+++ include/sound/soc.h
@@ -1158,7 +1158,6 @@ struct snd_soc_pcm_runtime {
 	struct list_head component_list; /* list of connected components */
 
 	/* bit field */
-	unsigned int dev_registered:1;
 	unsigned int pop_wait:1;
 	unsigned int fe_compr:1; /* for Dynamic PCM */
 };
