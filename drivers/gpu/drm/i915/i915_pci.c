e60d6f81f9f92266a9b19ffad8936ad5c0b3f1c6
Chris Wilson <chris@chris-wilson.co.uk>
2019-09-13 15:55:56 +0100

    UPSTREAM: drm/i915/tgl: Limit ourselves to just rcs0
    More pruning away of features until we have a stable system and a basis
    for debugging what's missing.
    
    v2: Fixup vdbox/vebox fusing
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190913145556.23912-1-chris@chris-wilson.co.uk
    (cherry picked from commit 6da301e5291717861fb25025b782922d3a36c7af)
    Signed-off-by: Chintan Patel <chintan.m.patel@intel.com>
    
    BUG=b:146438784, b:146143855, b:145022885, b:144953588
    TEST=Boot to graphics on TGL board provided Mesa Iris enabled see: b:141490430
    TEST=Run WebGL benchmark with 1k+ fish should not show blue fish
    TEST=Font should be clear when typing on login screen
    
    Change-Id: I68367bf973933e930d03f6d0e427472c4e1947ab

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -798,6 +798,7 @@ static const struct intel_device_info intel_tigerlake_12_info = {
 	.engine_mask =
 		BIT(RCS0) | BIT(BCS0) | BIT(VECS0) | BIT(VCS0) | BIT(VCS2),
 	.has_logical_ring_preemption = false, /* XXX disabled for debugging */
+	.engine_mask = BIT(RCS0), /* XXX reduced for debugging */
 };
 
 #undef GEN
