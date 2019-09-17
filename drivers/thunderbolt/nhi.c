6fac6a4521032728a20eb88b05bc8a0784b27997
Mika Westerberg <mika.westerberg@linux.intel.com>
2019-09-17 23:12:47 +0300

    CHROMIUM: thunderbolt: Add Power Management support for Tiger Lake
    Added suspend resume routines for Power Management support on
    Tiger Lake.
    Tiger Lake ES step does not have PREPARE_FOR_RESET implemented.
    Tiger Lake ES step also misses the correct USB4 version field so
    implemented stepping check around that.
    
    BUG=b:158284370
    BUG=b:158695046
    TEST=Run 500 S0ix Cycle with no device attached
    TOREVERT= This patch can be reverted once support of Tiger Lake ES is
    not needed anymore.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Change-Id: Iaa842a377bf5730c65ea1ffe0f4076d7130b20e0
    Signed-off-by: Utkarsh Patel <utkarsh.h.patel@intel.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2233541
    Reviewed-by: Prashant Malani <pmalani@chromium.org>

--- drivers/thunderbolt/nhi.c
+++ drivers/thunderbolt/nhi.c
@@ -1271,9 +1271,9 @@ static struct pci_device_id nhi_ids[] = {
 	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_ICL_NHI1),
 	  .driver_data = (kernel_ulong_t)&icl_nhi_ops },
 	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_TGL_NHI0),
-	  .driver_data = (kernel_ulong_t)&tgl_nhi_ops },
+	  .driver_data = (kernel_ulong_t)&icl_nhi_ops },
 	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_TGL_NHI1),
-	  .driver_data = (kernel_ulong_t)&tgl_nhi_ops },
+	  .driver_data = (kernel_ulong_t)&icl_nhi_ops },
 
 	/* Any USB4 compliant host */
 	{ PCI_DEVICE_CLASS(PCI_CLASS_SERIAL_USB_USB4, ~0) },
--- drivers/thunderbolt/nhi.h
+++ drivers/thunderbolt/nhi.h
@@ -49,7 +49,6 @@ struct tb_nhi_ops {
 };
 
 extern const struct tb_nhi_ops icl_nhi_ops;
-extern const struct tb_nhi_ops tgl_nhi_ops;
 
 /*
  * PCI IDs used in this driver from Win Ridge forward. There is no
