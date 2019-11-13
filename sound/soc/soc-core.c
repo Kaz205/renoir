fa2f3e81a1ec350e40fbff691ca50342b4162203
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-11-13 10:16:48 +0900

    UPSTREAM: ASoC: soc-core: care card_probed at soc_cleanup_card_resources()
    soc_cleanup_card_resources() will call card->remove(), but it should be
    called if card->probe() or card->late_probe() are called.
    snd_soc_bind_card() might be error before calling
    card->probe() / card->late_probe().
    In that time, card->remove() will be called.
    This patch adds card_probed parameter to judge it.
    
    Fixes: bfce78a559655 ("ASoC: soc-core: tidyup soc_init_dai_link()")
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Tested-by: Jon Hunter <jonathanh@nvidia.com>
    Link: https://lore.kernel.org/r/87o8xg4ltr.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit ce21401c9d30e81824e864dc25824ebeb9690801)
    Signed-off-by: Chintan Patel <chintan.m.patel@intel.com>
    
    BUG=b:147436144
    TEST=Audio playback via Speaker, Headphone and HDMI/DP
    
    Change-Id: I31c9c648956e3a1dec189ad5ca317173275b6f32
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2029809
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1942,7 +1942,8 @@ static void __soc_setup_card_name(char *name, int len,
 	}
 }
 
-static void soc_cleanup_card_resources(struct snd_soc_card *card)
+static void soc_cleanup_card_resources(struct snd_soc_card *card,
+				       int card_probed)
 {
 	struct snd_soc_dai_link *link, *_link;
 
@@ -1971,17 +1972,19 @@ static void soc_cleanup_card_resources(struct snd_soc_card *card)
 	soc_cleanup_card_debugfs(card);
 
 	/* remove the card */
-	if (card->remove)
+	if (card_probed && card->remove)
 		card->remove(card);
 }
 
 static void snd_soc_unbind_card(struct snd_soc_card *card, bool unregister)
 {
 	if (card->instantiated) {
+		int card_probed = 1;
+
 		card->instantiated = false;
 		snd_soc_flush_all_delayed_work(card);
 
-		soc_cleanup_card_resources(card);
+		soc_cleanup_card_resources(card, card_probed);
 		if (!unregister)
 			list_add(&card->list, &unbind_card_list);
 	} else {
@@ -1994,7 +1997,7 @@ static int snd_soc_bind_card(struct snd_soc_card *card)
 {
 	struct snd_soc_pcm_runtime *rtd;
 	struct snd_soc_dai_link *dai_link;
-	int ret, i;
+	int ret, i, card_probed = 0;
 
 	mutex_lock(&client_mutex);
 	mutex_lock_nested(&card->mutex, SND_SOC_CARD_CLASS_INIT);
@@ -2046,6 +2049,7 @@ static int snd_soc_bind_card(struct snd_soc_card *card)
 		ret = card->probe(card);
 		if (ret < 0)
 			goto probe_end;
+		card_probed = 1;
 	}
 
 	/* probe all components used by DAI links on this card */
@@ -2111,6 +2115,7 @@ static int snd_soc_bind_card(struct snd_soc_card *card)
 			goto probe_end;
 		}
 	}
+	card_probed = 1;
 
 	snd_soc_dapm_new_widgets(card);
 
@@ -2140,7 +2145,7 @@ static int snd_soc_bind_card(struct snd_soc_card *card)
 
 probe_end:
 	if (ret < 0)
-		soc_cleanup_card_resources(card);
+		soc_cleanup_card_resources(card, card_probed);
 
 	mutex_unlock(&card->mutex);
 	mutex_unlock(&client_mutex);
