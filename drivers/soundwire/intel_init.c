634a83365fe2dd6634a5eb6844b7101299efdeb0
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-12-05 14:15:27 -0600

    CHROMIUM: soundwire: intel_init: handle power rail dependencies for clock stop mode
    When none of the clock stop quirks is specified, the Master IP will
    assume the context is preserved and will not reset the Bus and restart
    enumeration. Due to power rail dependencies, the HDaudio controller
    needs to remain powered and prevented from executing its pm_runtime
    suspend routine.
    
    This choice of course has a power impact, and this mode should only be
    selected when latency requirements are critical or the parent device
    can enter D0ix modes.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    
    BUG=b:145960362
    TEST=Audio Playback via Speaker/Headset using Soundwire Daughter Card
    TOREVERT: Once upstream solution lands revert this.
    
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>

--- drivers/soundwire/intel_init.c
+++ drivers/soundwire/intel_init.c
@@ -14,6 +14,7 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/soundwire/sdw_intel.h>
+#include <linux/pm_runtime.h>
 #include "cadence_master.h"
 #include "intel.h"
 
@@ -72,6 +73,9 @@ static int sdw_intel_cleanup(struct sdw_intel_ctx *ctx)
 			pm_runtime_disable(&link->pdev->dev);
 			platform_device_unregister(link->pdev);
 		}
+
+		if (!link->clock_stop_quirks)
+			pm_runtime_put_noidle(link->dev);
 	}
 
 	return 0;
@@ -337,6 +341,16 @@ sdw_intel_startup_controller(struct sdw_intel_ctx *ctx)
 			continue;
 
 		intel_master_startup(link->pdev);
+
+		if (!link->clock_stop_quirks) {
+			/*
+			 * we need to prevent the parent PCI device
+			 * from entering pm_runtime suspend, so that
+			 * power rails to the SoundWire IP are not
+			 * turned off.
+			 */
+			pm_runtime_get_noresume(link->dev);
+		}
 	}
 
 	return 0;
