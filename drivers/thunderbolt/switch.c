baaf58c540e706367fe46840b1f0fd0241dc7ff7
Mika Westerberg <mika.westerberg@linux.intel.com>
2019-12-06 18:36:07 +0200

    FROMGIT: thunderbolt: Enable wakes from system suspend
    In order for the router and the whole domain to wake up from system
    suspend states we need to enable wakes for the connected routers. For
    device routers we enable wakes from PCIe and USB 3.x. This allows
    devices such as keyboards connected to USB 3.x hub that is tunneled to
    wake the system up as expected. For all routers we enabled wake on USB4
    for each connected ports. This is used to propagate the wake from router
    to another.
    
    Do the same for legacy routers through link controller vendor specific
    registers as documented in USB4 spec chapter 13.
    
    While there correct kernel-doc of usb4_switch_set_sleep() -- it does not
    enable wakes instead there is a separate function (usb4_switch_set_wake())
    that does.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    (cherry picked from commit b2911a593a705e54adde6d06d4657c1ff2f16583
    https://git.kernel.org/pub/scm/linux/kernel/git/westeri/thunderbolt.git next)
    
    BUG=b:162700590
    TEST=No regressions for the following use cases:
    - S0iX NDA/DA
    - TBT NVMe hotplug
    - TBT dock hotplug
    - TBT dock w/ USB pen drive
    - TBT dock w/ display
    
    Signed-off-by: Casey Bowman <casey.g.bowman@intel.com>
    Change-Id: I48c4f117a4a6737ca45ce3c43270e930016e01f9

--- drivers/thunderbolt/switch.c
+++ drivers/thunderbolt/switch.c
@@ -2036,7 +2036,7 @@ int tb_switch_configure(struct tb_switch *sw)
 	route = tb_route(sw);
 
 	tb_dbg(tb, "%s Switch at %#llx (depth: %d, up port: %d)\n",
-	       sw->config.enabled ? "restoring " : "initializing", route,
+	       sw->config.enabled ? "restoring" : "initializing", route,
 	       tb_route_length(route), sw->config.upstream_port_number);
 
 	sw->config.enabled = 1;
@@ -2502,6 +2502,13 @@ int tb_switch_add(struct tb_switch *sw)
 		return ret;
 	}
 
+	/*
+	 * Thunderbolt routers do not generate wakeups themselves but
+	 * they forward wakeups from tunneled protocols, so enable it
+	 * here.
+	 */
+	device_init_wakeup(&sw->dev, true);
+
 	pm_runtime_set_active(&sw->dev);
 	if (sw->rpm) {
 		pm_runtime_set_autosuspend_delay(&sw->dev, TB_AUTOSUSPEND_DELAY);
@@ -2579,6 +2586,18 @@ void tb_sw_set_unplugged(struct tb_switch *sw)
 	}
 }
 
+static int tb_switch_set_wake(struct tb_switch *sw, unsigned int flags)
+{
+	if (flags)
+		tb_sw_dbg(sw, "enabling wakeup: %#x\n", flags);
+	else
+		tb_sw_dbg(sw, "disabling wakeup\n");
+
+	if (tb_switch_is_usb4(sw))
+		return usb4_switch_set_wake(sw, flags);
+	return tb_lc_set_wake(sw, flags);
+}
+
 int tb_switch_resume(struct tb_switch *sw)
 {
 	struct tb_port *port;
@@ -2624,6 +2643,9 @@ int tb_switch_resume(struct tb_switch *sw)
 	if (err)
 		return err;
 
+	/* Disable wakes */
+	tb_switch_set_wake(sw, 0);
+
 	err = tb_switch_tmu_init(sw);
 	if (err)
 		return err;
@@ -2659,6 +2681,7 @@ int tb_switch_resume(struct tb_switch *sw)
 
 void tb_switch_suspend(struct tb_switch *sw)
 {
+	unsigned int flags = 0;
 	struct tb_port *port;
 	int err;
 
@@ -2670,6 +2693,11 @@ void tb_switch_suspend(struct tb_switch *sw)
 		if (tb_port_has_remote(port))
 			tb_switch_suspend(port->remote->sw);
 	}
+
+	if (device_may_wakeup(&sw->dev))
+		flags = TB_WAKE_ON_USB4 | TB_WAKE_ON_USB3 | TB_WAKE_ON_PCIE;
+
+	tb_switch_set_wake(sw, flags);
 
 	if (tb_switch_is_usb4(sw))
 		usb4_switch_set_sleep(sw);
