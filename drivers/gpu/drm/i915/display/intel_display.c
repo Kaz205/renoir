5f6624781bf2c8f55541de00d4f3e890b8ef9b31
Ville Syrjälä <ville.syrjala@linux.intel.com>
2020-01-15 21:08:13 +0200

    UPSTREAM: drm/i915: Move encoder variable to tighter scope
    Let's not pollute the function scope with variables when they're
    only needed inside some loops.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200115190813.17971-5-ville.syrjala@linux.intel.com
    Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
    (cherry picked from commit 691313ea621482aa3da9721770b2642f789f63e5)
    
    
    
    BUG=b:168131866
    TEST=Compile tested/Backported fixes identified with r-o-m/On-device testing with eve-kernelnext
    
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Change-Id: I3fcde5917cc07b2ee0b3c390fbd75f60ef79d8f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2405104
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -13086,7 +13086,6 @@ intel_modeset_pipe_config(struct intel_crtc_state *pipe_config)
 {
 	struct drm_crtc *crtc = pipe_config->uapi.crtc;
 	struct drm_atomic_state *state = pipe_config->uapi.state;
-	struct intel_encoder *encoder;
 	struct drm_connector *connector;
 	struct drm_connector_state *connector_state;
 	int base_bpp, ret;
@@ -13129,10 +13128,11 @@ intel_modeset_pipe_config(struct intel_crtc_state *pipe_config)
 			       &pipe_config->pipe_src_h);
 
 	for_each_new_connector_in_state(state, connector, connector_state, i) {
+		struct intel_encoder *encoder =
+			to_intel_encoder(connector_state->best_encoder);
+
 		if (connector_state->crtc != crtc)
 			continue;
-
-		encoder = to_intel_encoder(connector_state->best_encoder);
 
 		if (!check_single_encoder_cloning(state, to_intel_crtc(crtc), encoder)) {
 			DRM_DEBUG_KMS("rejecting invalid cloning configuration\n");
@@ -13183,6 +13183,9 @@ encoder_retry:
 	 * a chance to reject the mode entirely.
 	 */
 	for_each_new_connector_in_state(state, connector, connector_state, i) {
+		struct intel_encoder *encoder =
+			to_intel_encoder(connector_state->best_encoder);
+
 		if (connector_state->crtc != crtc)
 			continue;
 
@@ -13194,7 +13197,6 @@ encoder_retry:
 			return ret;
 		}
 
-		encoder = to_intel_encoder(connector_state->best_encoder);
 		ret = encoder->compute_config(encoder, pipe_config,
 					      connector_state);
 		if (ret < 0) {
