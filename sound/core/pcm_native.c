fea7ff6c9bebf2f26d0ede2485b01adebcfd5c2e
Takashi Iwai <tiwai@suse.de>
2020-01-16 17:28:25 +0100

    UPSTREAM: ALSA: pcm: Make snd_pcm_hw_constraints_init() and _complete() static
    Both snd_pcm_hw_constraints_init() and _complete() functions are
    called only from pcm_native.c, hence they can be static for further
    optimization.
    
    Link: https://lore.kernel.org/r/20200116162825.24792-1-tiwai@suse.de
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit d662117c7a91b4f4db1d5458fe782cdf54f8359a)
    Signed-off-by: Chintan Patel <chintan.m.patel@intel.com>
    
    BUG=b:149102665
    TEST=Audio playback via HDMI/DP
    TEST=Suspend to S0iX with audio playback with right coreboot
    
    Change-Id: I6345b7cf7ab46a5d62bd5b4b3688f0205722729c
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2070466
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Tested-by: Minh QuangX Nguyen <minh.quangx.nguyen@intel.com>

--- sound/core/pcm_native.c
+++ sound/core/pcm_native.c
@@ -2253,7 +2253,7 @@ static int snd_pcm_hw_rule_buffer_bytes_max(struct snd_pcm_hw_params *params,
 	return snd_interval_refine(hw_param_interval(params, rule->var), &t);
 }		
 
-int snd_pcm_hw_constraints_init(struct snd_pcm_substream *substream)
+static int snd_pcm_hw_constraints_init(struct snd_pcm_substream *substream)
 {
 	struct snd_pcm_runtime *runtime = substream->runtime;
 	struct snd_pcm_hw_constraints *constrs = &runtime->hw_constraints;
@@ -2377,7 +2377,7 @@ int snd_pcm_hw_constraints_init(struct snd_pcm_substream *substream)
 	return 0;
 }
 
-int snd_pcm_hw_constraints_complete(struct snd_pcm_substream *substream)
+static int snd_pcm_hw_constraints_complete(struct snd_pcm_substream *substream)
 {
 	struct snd_pcm_runtime *runtime = substream->runtime;
 	struct snd_pcm_hardware *hw = &runtime->hw;
