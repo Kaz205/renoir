6e7050da2bb220b60da774c9fef44a13222fd03a
Benjamin Gaignard <benjamin.gaignard@st.com>
2020-01-20 14:33:59 +0100

    UPSTREAM: cpuidle: sysfs: fix warnings when compiling with W=1
    Fix kernel documentation comments to remove warnings when
    compiling with W=1.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@st.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit a09da3fbc17f36feac00fea37a225596ae302360)
    
    BUG=b:146144745
    TEST=Kernel boots on volteer
    
    Signed-off-by: Ed Baker <edward.baker@intel.com>
    Change-Id: I2d78f5a1985befc7c5ff01df41e8129285d3276d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2092337
    Reviewed-by: Alex Levin <levinale@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/cpuidle/sysfs.c
+++ drivers/cpuidle/sysfs.c
@@ -142,6 +142,7 @@ static struct attribute_group cpuidle_attr_group = {
 
 /**
  * cpuidle_add_interface - add CPU global sysfs attributes
+ * @dev: the target device
  */
 int cpuidle_add_interface(struct device *dev)
 {
@@ -153,6 +154,7 @@ int cpuidle_add_interface(struct device *dev)
 
 /**
  * cpuidle_remove_interface - remove CPU global sysfs attributes
+ * @dev: the target device
  */
 void cpuidle_remove_interface(struct device *dev)
 {
@@ -625,7 +627,7 @@ static struct kobj_type ktype_driver_cpuidle = {
 
 /**
  * cpuidle_add_driver_sysfs - adds the driver name sysfs attribute
- * @device: the target device
+ * @dev: the target device
  */
 static int cpuidle_add_driver_sysfs(struct cpuidle_device *dev)
 {
@@ -656,7 +658,7 @@ static int cpuidle_add_driver_sysfs(struct cpuidle_device *dev)
 
 /**
  * cpuidle_remove_driver_sysfs - removes the driver name sysfs attribute
- * @device: the target device
+ * @dev: the target device
  */
 static void cpuidle_remove_driver_sysfs(struct cpuidle_device *dev)
 {
