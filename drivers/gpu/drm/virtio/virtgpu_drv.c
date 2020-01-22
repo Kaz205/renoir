f65fc97508c14b41538d06dfdbf2630d56548814
David Stevens <stevensd@chromium.org>
2020-01-22 21:07:05 +0900

    BACKPORT: FROMLIST: drm/virtio: Support virtgpu exported resources
    Add support for UUID-based resource sharing mechanism to virtgpu. This
    implements the new virtgpu commands and hooks them up to dma-buf's
    get_uuid callback.
    
    Signed-off-by: David Stevens <stevensd@chromium.org>
    (am from https://patchwork.kernel.org/patch/11431393/)
    
    Fixes:
     - Shove virtio_gpu_object in data_buf since objs isn't available
     - Remove virtio specific dma_buf_ops, but make sure that
       virtio_gpu_dma_buf_to_handle continues to work. This effectively
       reverts b7f70332c5c52f614641a33418b9b2a16204f4e6.
    
    BUG=b:136269340
    TEST=boot ARCVM and launch play store
    
    Change-Id: Iaadd072243a1d7a8208ba18ad4d8d3aa22996e9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2059087
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Commit-Queue: David Stevens <stevensd@chromium.org>
    Tested-by: David Stevens <stevensd@chromium.org>

--- drivers/gpu/drm/virtio/virtgpu_drv.c
+++ drivers/gpu/drm/virtio/virtgpu_drv.c
@@ -164,6 +164,7 @@ static unsigned int features[] = {
 	VIRTIO_GPU_F_VIRGL,
 #endif
 	VIRTIO_GPU_F_EDID,
+	VIRTIO_GPU_F_RESOURCE_UUID,
 	VIRTIO_GPU_F_RESOURCE_V2,
 	VIRTIO_GPU_F_SHARED_GUEST,
 	VIRTIO_GPU_F_HOST_COHERENT,
@@ -214,12 +215,12 @@ static struct drm_driver driver = {
 	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
 	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
 	.gem_prime_export = virtgpu_gem_prime_export,
-	.gem_prime_import = virtgpu_gem_prime_import,
 	.gem_prime_get_sg_table = virtgpu_gem_prime_get_sg_table,
 	.gem_prime_import_sg_table = virtgpu_gem_prime_import_sg_table,
 	.gem_prime_vmap = virtgpu_gem_prime_vmap,
 	.gem_prime_vunmap = virtgpu_gem_prime_vunmap,
 	.gem_prime_mmap = virtgpu_gem_prime_mmap,
+	.gem_prime_get_uuid = virtgpu_gem_prime_get_uuid,
 
 	.gem_free_object_unlocked = virtio_gpu_gem_free_object,
 	.gem_open_object = virtio_gpu_gem_object_open,
--- drivers/gpu/drm/virtio/virtgpu_object.c
+++ drivers/gpu/drm/virtio/virtgpu_object.c
@@ -27,6 +27,7 @@
 
 #include <drm/ttm/ttm_execbuf_util.h>
 #include <linux/dma-buf.h>
+#include <linux/uuid.h>
 #include "virtgpu_drv.h"
 
 static int virtio_gpu_virglrenderer_workaround = 1;
@@ -293,8 +294,9 @@ int virtio_gpu_dma_buf_to_handle(struct dma_buf *dma_buf, bool no_wait,
 {
 	struct virtio_gpu_object *qobj;
 	struct virtio_gpu_device *vgdev;
+	uuid_t uuid;
 
-	if (dma_buf->ops != &virtgpu_dmabuf_ops)
+	if (dma_buf_get_uuid(dma_buf, &uuid) != 0)
 		return -EINVAL;
 
 	qobj = gem_to_virtio_gpu_obj(dma_buf->priv);
