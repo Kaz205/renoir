3f0082cb2d20f418217ee0a0ff1afc8cc896f021
Zhan Liu <zhan.liu@amd.com>
2020-01-28 16:38:53 -0500

    BACKPORT: UPSTREAM: drm/amd/display: Move drm_dp_mst_atomic_check() to the front of dc_validate_global_state()
    [Why]
    Need to do atomic check first, then validate global state.
    If not, when connecting both MST and HDMI displays and
    set a bad mode via xrandr, system will hang.
    
    [How]
    Move drm_dp_mst_atomic_check() to the front of
    dc_validate_global_state().
    
    Signed-off-by: Zhan Liu <zhan.liu@amd.com>
    Reviewed-by: Mikita Lipski <mikita.lipski@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit ded58c7bbbf3b52c018db99fd4d530c3448a0c83)
    
    Changes were Minor rebase issues addressed
    
    TEST=Connect 4K monitor to dock and connect dock to DUT
    BUG=b:161144802
    
    Change-Id: I47dd2d240a9c98491928eb543cbc03d89c77f0c2
    Signed-off-by: Pratik Vishwakarma <pratik.vishwakarma@amd.corp-partner.google.com>
    Signed-off-by: Shirish S <shirish.s@amd.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2345986
    Reviewed-by: Drew Davenport <ddavenport@chromium.org>
    Commit-Queue: Drew Davenport <ddavenport@chromium.org>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -7816,6 +7816,16 @@ static int amdgpu_dm_atomic_check(struct drm_device *dev,
 		if (ret)
 			goto fail;
 
+		/*
+		 * Perform validation of MST topology in the state:
+		 * We need to perform MST atomic check before calling
+		 * dc_validate_global_state(), or there is a chance
+		 * to get stuck in an infinite loop and hang eventually.
+		 */
+		ret = drm_dp_mst_atomic_check(state);
+		if (ret)
+			goto fail;
+
 		if (dc_validate_global_state(dc, dm_state->context, false) != DC_OK) {
 			ret = -EINVAL;
 			goto fail;
