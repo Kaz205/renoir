4f9d7f20771960310ce8e2d8f0bc18f172cdeaf1
Curtis Malainey <cujomalainey@chromium.org>
2020-02-05 12:26:40 -0800

    Revert "ASoC: SOF: topology: Fix bytes control size checks"
    This reverts commit 9508ef5a980f5d847cad9b932b6ada8f2a3466c1.
    
    This is causing upstream commits to be backported, reverting and
    relanding upstream version
    
    BUG=b:147436144
    TEST=Audio playback via Speaker, Headphone and HDMI/DP
    
    Cq-Depend: chromium:2039955
    Change-Id: I45e1f2b1ac9242efeaf8e49cedc51975e0e5f9cd
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2039954
    Reviewed-by: Alex Levin <levinale@chromium.org>

--- sound/soc/sof/topology.c
+++ sound/soc/sof/topology.c
@@ -543,16 +543,15 @@ static int sof_control_load_bytes(struct snd_soc_component *scomp,
 	struct soc_bytes_ext *sbe = (struct soc_bytes_ext *)kc->private_value;
 	int max_size = sbe->max;
 
-	/* init the get/put bytes data */
-	scontrol->size = sizeof(struct sof_ipc_ctrl_data) +
-		le32_to_cpu(control->priv.size);
-
-	if (scontrol->size > max_size) {
+	if (le32_to_cpu(control->priv.size) > max_size) {
 		dev_err(sdev->dev, "err: bytes data size %d exceeds max %d.\n",
-			scontrol->size, max_size);
+			control->priv.size, max_size);
 		return -EINVAL;
 	}
 
+	/* init the get/put bytes data */
+	scontrol->size = sizeof(struct sof_ipc_ctrl_data) +
+		le32_to_cpu(control->priv.size);
 	scontrol->control_data = kzalloc(max_size, GFP_KERNEL);
 	cdata = scontrol->control_data;
 	if (!scontrol->control_data)
