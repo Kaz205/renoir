87b6345a533a504abbae05e059d8bf385f789b3e
Pankaj Bharadiya <pankaj.laxminarayan.bharadiya@intel.com>
2020-03-07 14:00:20 +0530

    UPSTREAM: drm: Remove dp mst register connector callbacks
    drm_dp_mst_port_add_connector() directly calls the
    drm_connector_register() now and
    drm_dp_mst_topology_mgr_cbs.register_connector callback is not getting
    called anymore.
    
    Hence remove all drm_dp_mst_topology_mgr_cbs.register_connector
    callbacks.
    
    This is the preparatory step for removing the
    drm_dp_mst_topology_mgr_cbs.register_connector callback hook.
    
    The removal is done with below sementic patch:
    
    @r1@
    identifier func, E;
    @@
    struct drm_dp_mst_topology_cbs E = {
    	...,
    -        .register_connector = func
    };
    
    @delete depends on r1@
    identifier r1.func;
    @@
    - static void func(...){...}
    
    Signed-off-by: Pankaj Bharadiya <pankaj.laxminarayan.bharadiya@intel.com>
    Suggested-by: Emil Velikov <emil.velikov@collabora.com>
    Suggested-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200307083023.76498-3-pankaj.laxminarayan.bharadiya@intel.com
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    (cherry picked from commit 615eff35d4278a7701ea36a7808ff5ba2f3d8bac)
    
    
    BUG=b:170142088
    TEST=Compile tested/Backported fixes identified with r-o-m/On-device testing with eve-kernelnext
    
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Change-Id: I5e6a114cb5b62a852d3393f81de14b2897b74dbc

--- drivers/gpu/drm/i915/display/intel_dp_mst.c
+++ drivers/gpu/drm/i915/display/intel_dp_mst.c
@@ -749,11 +749,6 @@ err:
 	return NULL;
 }
 
-static void intel_dp_register_mst_connector(struct drm_connector *connector)
-{
-	drm_connector_register(connector);
-}
-
 static void intel_dp_destroy_mst_connector(struct drm_dp_mst_topology_mgr *mgr,
 					   struct drm_connector *connector)
 {
@@ -773,7 +768,6 @@ intel_dp_mst_poll_hpd_irq(struct drm_dp_mst_topology_mgr *mgr)
 
 static const struct drm_dp_mst_topology_cbs mst_cbs = {
 	.add_connector = intel_dp_add_mst_connector,
-	.register_connector = intel_dp_register_mst_connector,
 	.destroy_connector = intel_dp_destroy_mst_connector,
 	.poll_hpd_irq = intel_dp_mst_poll_hpd_irq,
 };
