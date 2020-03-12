d7e2f3997581e202e414caec61385db234927e81
Prashant Malani <pmalani@chromium.org>
2020-03-12 11:34:24 -0700

    CHROMIUM: extcon: Use PD-notifier
    Switch to using the PD-notifier driver to receive PD event notifications
    from the Chrome EC.
    
    BUG=b:147758849
    BUG=b:147460168
    TEST=None
    
    Cq-Depend: chromium:2101568
    Signed-off-by: Prashant Malani <pmalani@chromium.org>
    Change-Id: I319fcea5d93166b6674241f3a1e4adc0aa4f0c10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2101570
    Reviewed-by: Benson Leung <bleung@google.com>
    Reviewed-by: Brandon Breitenstein <brandon.breitenstein@intel.com>

--- drivers/extcon/extcon-tcss-cros-ec.c
+++ drivers/extcon/extcon-tcss-cros-ec.c
@@ -13,6 +13,7 @@
 
 #include <linux/module.h>
 #include <linux/platform_data/cros_ec_proto.h>
+#include <linux/platform_data/cros_usbpd_notify.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/usb/typec_dp.h>
@@ -528,8 +529,7 @@ static int cros_ec_tcss_remove(struct platform_device *pdev)
 {
 	struct cros_ec_tcss_info *info = platform_get_drvdata(pdev);
 
-	blocking_notifier_chain_unregister(&info->ec->event_notifier,
-					   &info->notifier);
+	cros_usbpd_unregister_notify(&info->notifier);
 	cancel_work_sync(&info->bh_work);
 	mutex_destroy(&info->lock);
 
@@ -582,8 +582,7 @@ static int cros_ec_tcss_probe(struct platform_device *pdev)
 
 	/* Get Port detection events from the EC */
 	info->notifier.notifier_call = cros_ec_tcss_event;
-	ret = blocking_notifier_chain_register(&info->ec->event_notifier,
-					       &info->notifier);
+	ret = cros_usbpd_register_notify(&info->notifier);
 	if (ret < 0) {
 		dev_err(dev, "failed to register notifier\n");
 		goto destroy_mutex;
@@ -611,8 +610,7 @@ return 0;
 
 remove_tcss:
 	mutex_unlock(&info->lock);
-	blocking_notifier_chain_unregister(&info->ec->event_notifier,
-					   &info->notifier);
+	cros_usbpd_unregister_notify(&info->notifier);
 	cancel_work_sync(&info->bh_work);
 destroy_mutex:
 	mutex_destroy(&info->lock);
