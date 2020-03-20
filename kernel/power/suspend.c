27c8339bc0b1f1fbab178a513008dc6525ff5574
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2020-03-20 13:28:35 +0100

    swait: Remove the warning with more than two waiters
    The warning was introduced to find callers of complete_all() having
    multiple waiters enqueued. As completa_all() wakes all waiters with
    disabled interrupts it may lead visibile latency spikes with a larger
    amount of waiters.
    
    Since the warning was introduced, most of the feedback was in the
    "setup/configure" phase which is nothing that would disturb the RT
    workload because it is not yet active.
    There were reports regarding the crypto code which may wake multiple
    waiters if all of them request an algorithm which requires a module to
    be loaded first. If this really become a problem during runtime it
    should be investigated then.
    
    Remove the warning if more than two waiters are worken up via the
    complete_all() interface.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -610,8 +610,6 @@ static int enter_state(suspend_state_t state)
 	return error;
 }
 
-bool pm_in_action;
-
 /**
  * pm_suspend - Externally visible function for suspending the system.
  * @state: System sleep state to enter.
@@ -626,7 +624,6 @@ int pm_suspend(suspend_state_t state)
 	if (state <= PM_SUSPEND_ON || state >= PM_SUSPEND_MAX)
 		return -EINVAL;
 
-	pm_in_action = true;
 	pr_debug("suspend entry (%s)\n", mem_sleep_labels[state]);
 	error = enter_state(state);
 	if (error) {
@@ -636,7 +633,6 @@ int pm_suspend(suspend_state_t state)
 		suspend_stats.success++;
 	}
 	pr_debug("suspend exit\n");
-	pm_in_action = false;
 	return error;
 }
 EXPORT_SYMBOL(pm_suspend);
