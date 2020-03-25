c8c4b05388e6fe4175d9d95d4cee1be018fba91f
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2020-03-25 15:03:35 +0100

    UPSTREAM: cpufreq: intel_pstate: Use passive mode by default without HWP
    After recent changes allowing scale-invariant utilization to be
    used on x86, the schedutil governor on top of intel_pstate in the
    passive mode should be on par with (or better than) the active mode
    "powersave" algorithm of intel_pstate on systems in which
    hardware-managed P-states (HWP) are not used, so it should not be
    necessary to use the internal scaling algorithm in those cases.
    
    Accordingly, modify intel_pstate to start in the passive mode by
    default if the processor at hand does not support HWP of if the driver
    is requested to avoid using HWP through the kernel command line.
    
    Among other things, that will allow utilization clamps and the
    support for RT/DL tasks in the schedutil governor to be utilized on
    systems in which intel_pstate is used.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 33aa46f252c703e42c81a76696cd0c240f2281e4)
    
    BUG=b:135666007
    TEST=Verify the sysfs interface with different values
    
    Signed-off-by: Ed Baker <edward.baker@intel.com>
    Signed-off-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
    Change-Id: I28ece4715a1e3f161f637443ad098337f10ce295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2444962
    Reviewed-by: Evan Green <evgreen@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Commit-Queue: Evan Green <evgreen@chromium.org>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -2782,6 +2782,8 @@ static int __init intel_pstate_init(void)
 		pr_info("Invalid MSRs\n");
 		return -ENODEV;
 	}
+	/* Without HWP start in the passive mode. */
+	default_driver = &intel_cpufreq;
 
 hwp_cpu_matched:
 	/*
@@ -2827,7 +2829,6 @@ static int __init intel_pstate_setup(char *str)
 	if (!strcmp(str, "disable")) {
 		no_load = 1;
 	} else if (!strcmp(str, "passive")) {
-		pr_info("Passive mode enabled\n");
 		default_driver = &intel_cpufreq;
 		no_hwp = 1;
 	}
