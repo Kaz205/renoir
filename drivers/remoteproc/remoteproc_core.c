29b172ce74b311c30ff9aba0748b93d2ac01ad74
Mathieu Poirier <mathieu.poirier@linaro.org>
2020-04-20 17:15:58 -0600

    FROMGIT: remoteproc: Use kstrdup_const() rather than kstrdup()
    For cases where @firmware is declared "const char *", use function
    kstrdup_const() to avoid needlessly creating another copy on the
    heap.
    
    Suggested-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Reviewed-by: Alex Elder <elder@linaro.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/20200420231601.16781-2-mathieu.poirier@linaro.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    (cherry picked from commit 1487deda19c82d30d1867277e89bc2d515b9d2d4
     https://git.kernel.org/pub/scm/linux/kernel/git/andersson/remoteproc.git/ rproc-next)
    
    BUG=b:149924251
    TEST=Boot trogdor with wifi/modem
    
    Signed-off-by: Evan Green <evgreen@chromium.org>
    Change-Id: Ieed086b023997aca722a075fef669f2c635505aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2169285
    Commit-Queue: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -1997,7 +1997,7 @@ static void rproc_type_release(struct device *dev)
 	if (rproc->index >= 0)
 		ida_simple_remove(&rproc_dev_index, rproc->index);
 
-	kfree(rproc->firmware);
+	kfree_const(rproc->firmware);
 	kfree(rproc->ops);
 	kfree(rproc);
 }
@@ -2010,7 +2010,7 @@ static const struct device_type rproc_type = {
 static int rproc_alloc_firmware(struct rproc *rproc,
 				const char *name, const char *firmware)
 {
-	char *p;
+	const char *p;
 
 	if (!firmware)
 		/*
@@ -2019,7 +2019,7 @@ static int rproc_alloc_firmware(struct rproc *rproc,
 		 */
 		p = kasprintf(GFP_KERNEL, "rproc-%s-fw", name);
 	else
-		p = kstrdup(firmware, GFP_KERNEL);
+		p = kstrdup_const(firmware, GFP_KERNEL);
 
 	if (!p)
 		return -ENOMEM;
@@ -2123,7 +2123,7 @@ struct rproc *rproc_alloc(struct device *dev, const char *name,
 	return rproc;
 
 free_firmware:
-	kfree(rproc->firmware);
+	kfree_const(rproc->firmware);
 free_rproc:
 	kfree(rproc);
 	return NULL;
