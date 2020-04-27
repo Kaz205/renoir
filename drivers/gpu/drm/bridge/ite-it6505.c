f04caae8140a5e06479580f8480a9d7f0895334c
Hsin-Yi Wang <hsinyi@chromium.org>
2020-04-27 17:26:38 +0800

    FIXUP: drm/bridge: it6505: include drm_bridge.h
    Fix up it6505 for ee68c743f8d0 ("drm: Stop including drm_bridge.h from drm_crtc.h")
    
    BUG=b:155410017
    TEST=build
    
    Change-Id: I6f39002f44149ecfd4097e36605f9e08e95cf60f
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2187486
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/gpu/drm/bridge/ite-it6505.c
+++ drivers/gpu/drm/bridge/ite-it6505.c
@@ -21,6 +21,7 @@
 #include <crypto/sha.h>
 
 #include <drm/drm_atomic_helper.h>
+#include <drm/drm_bridge.h>
 #include <drm/drm_crtc.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_dp_helper.h>
