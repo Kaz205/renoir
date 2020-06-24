acd8618c1b0306d201b32a10d7c482f427817dbd
Prashant Malani <pmalani@chromium.org>
2020-06-24 01:09:23 -0700

    BACKPORT: FROMGIT: platform/chrome: cros_ec: Add TBT pd_ctrl fields
    To support Thunderbolt compatibility mode, synchronize
    ec_response_usb_pd_control_v2 with the Chrome EC version, so that
    we get the Thunderbolt related control fields and macros.
    
    BUG=b:140645106
    TEST=Build and boot volteer with TCPMv1, verify that TBT docks can be
    enumerated across multiple hotplugs.
    
    Signed-off-by: Prashant Malani <pmalani@chromium.org>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    (cherry picked from commit 5e48a03bb9bff1728164040d71aa03cdb3cdfca2 git://git.kernel.org/pub/scm/linux/kernel/git/chrome-platform/linux.git for-next)
    Signed-off-by: Prashant Malani <pmalani@chromium.org>
    Change-Id: I68df2aad227a5883b25f8061b98b59b2d04f17ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2271130
    Reviewed-by: Alex Levin <levinale@google.com>
    Reviewed-by: Benson Leung <bleung@google.com>

--- include/linux/platform_data/cros_ec_commands.h
+++ include/linux/platform_data/cros_ec_commands.h
@@ -4946,13 +4946,12 @@ struct ec_response_usb_pd_control_v2 {
 	uint8_t role;
 	uint8_t polarity;
 	char state[32];
-	uint8_t cc_state; /* USBC_PD_CC_*Encoded cc state */
-	uint8_t dp_mode;  /* Current DP pin mode (MODE_DP_PIN_[A-E]) */
-	/* CL:1500994 Current cable type */
-	uint8_t reserved_cable_type;
-	uint8_t control_flags;  /* USB_PD_CTRL_*flags */
-	uint8_t cable_speed;    /* TBT_SS_* cable speed */
-	uint8_t cable_gen;      /* TBT_GEN3_* cable rounded support */
+	uint8_t cc_state;	/* enum pd_cc_states representing cc state */
+	uint8_t dp_mode;	/* Current DP pin mode (MODE_DP_PIN_[A-E]) */
+	uint8_t reserved;	/* Reserved for future use */
+	uint8_t control_flags;	/* USB_PD_CTRL_*flags */
+	uint8_t cable_speed;	/* TBT_SS_* cable speed */
+	uint8_t cable_gen;	/* TBT_GEN3_* cable rounded support */
 } __ec_align1;
 
 #define EC_CMD_USB_PD_PORTS 0x0102
