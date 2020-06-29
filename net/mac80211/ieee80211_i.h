3cfd8367445e82d6da5d302e443966336cf2f115
Loic Poulain <loic.poulain@linaro.org>
2020-06-29 10:55:23 +0200

    UPSTREAM: mac80211: Inform AP when returning operating channel
    Because we can miss AP wakeup (beacon) while scanning other channels,
    it's better go into wakeup state and inform the AP of that upon
    returning to the operating channel, rather than staying asleep and
    waiting for the next TIM indicating traffic for us.
    
    This saves precious time, especially when we only have 200ms inter-
    scan period for monitoring the active connection.
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>
    Link: https://lore.kernel.org/r/1593420923-26668-1-git-send-email-loic.poulain@linaro.org
    [rewrite commit message a bit]
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry picked from commit dba0491f00114b9f688bfff60545e90a270023e1)
    
    BUG=b:176671555
    TEST=none
    
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Change-Id: I39cace2e4d82217b2dd4a12b9a235b2bcfddd736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2651992
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Tested-by: Brian Norris <briannorris@chromium.org>
    Commit-Queue: Brian Norris <briannorris@chromium.org>

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -1352,7 +1352,6 @@ struct ieee80211_local {
 				*/
 
 	bool pspolling;
-	bool offchannel_ps_enabled;
 	/*
 	 * PS can only be enabled when we have exactly one managed
 	 * interface (and monitors) in PS, this then points there.
