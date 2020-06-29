e0432164b982fbdfe011a6586c93e3ac4e9f5f24
Douglas Anderson <dianders@chromium.org>
2020-06-29 12:37:02 -0700

    CHROMIUM: sched: Don't uclamp auto-boost RT threads to max freq
    Right now, if uclamp is enabled (which it is on Chrome OS ARM boards)
    then real time tasks will automatically be boosted to the top CPU
    frequency.  The assumption is that real time tasks should get done as
    soon as possible and thus we should burn whatever power we can do get
    them done.
    
    This is probably not the right behavior for Chrome OS.  Very little is
    so urgent on Chrome OS that it needs to burn max power to run.  If we
    find something, the right answer is likely to boost just that one
    thing.
    
    Also note: uclamp is currently only enabled on ARM boards, so if the
    boosting was truly important then we should probably have enabled it
    for x86 as well.
    
    Examples where the boosting is actively harmful:
    - cros_ec_spi uses a RT thread for its transfers.  This is because the
      EC will drop transfers if they take too long and we currently have
      no reliable retry mechanism.  We moved to RT not because we needed
      max CPU speed but to prevent other tasks from preempting us at a bad
      time.  Since we use cros_ec_spi for A LOT the boost in frequency was
      hitting power consumption quite hard.
    - daisydog, our userspace watchdog patting code, runs at RT.  Again,
      it doesn't need any special CPU frequency but is RT because it wants
      to make sure other tasks in the system don't starve it.  Boosting up
      cpufreq only burns power.
    
    Proposing this as a CHROMIUM patch because upstream is already working
    on this and we'd like a temporary solution while they debate the finer
    points.  References:
    
    * https://lore.kernel.org/r/20200511154053.7822-1-qais.yousef@arm.com/
    * https://lore.kernel.org/r/20200610151818.1.I666ecd9c6f3c6405bd75831a21001b8109b6438c@changeid
    
    A CHROMIUM patch likely makes more sense than a FROMLIST in this case
    because:
    - It's really simple.
    - It's self contained and easy to revert.
    
    If we were to pick the upstream patch FROMLIST we'd need to add
    something to our sysctl settings to tweak it and if the name of the
    sysctl (or the usage of it) changed by the time it landed upstream
    it'd be more churn/hassle to handle it.
    
    This patch should almost certainly not be rebased to the next LTS
    kernel.  If things are still not resolved by EOY we need to push
    harder for a solution to land sooner rather than later upstream.
    
    BUG=b:157888348, b:160171130
    TEST=Check power consumption after this patch
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Change-Id: Ib70487ffc2e60ce111511f10c8caf7cc86a79553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2274340
    Reviewed-by: Joel Fernandes <joelaf@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1201,10 +1201,6 @@ static void __setscheduler_uclamp(struct task_struct *p,
 		if (uc_se->user_defined)
 			continue;
 
-		/* By default, RT tasks always get 100% boost */
-		if (unlikely(rt_task(p) && clamp_id == UCLAMP_MIN))
-			clamp_value = uclamp_none(UCLAMP_MAX);
-
 		uclamp_se_set(uc_se, clamp_value, false);
 	}
 
