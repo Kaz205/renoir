4e186ff60c11535b22b7f81c85606260e363989f
Sudeep Holla <sudeep.holla@arm.com>
2020-07-09 16:49:31 +0100

    UPSTREAM: drm/i915/selftests: Fix compare functions provided for sorting
    Both cmp_u32 and cmp_u64 are comparing the pointers instead of the value
    at those pointers. This will result in incorrect/unsorted list. Fix it
    by deferencing the pointers before comparison.
    
    Fixes: 4ba74e53ada3 ("drm/i915/selftests: Verify frequency scaling with RPS")
    Fixes: 8757797ff9c9 ("drm/i915/selftests: Repeat the rps clock frequency measurement")
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200709154931.23310-1-sudeep.holla@arm.com
    (cherry picked from commit 2196dfea896f7027b43bae848890ce4aec5c8724)
    
    BUG=b:168669388
    TEST=Check for GPU frequency samples above 1100MHz.
         - Login as guest and launch WebGL Aquarium with 30,000 fish.
         - Monitor frequency statistics.
           `socwatch -f cpu -f gfx -t 120`
           alternatively
           `while true; do cat /sys/class/drm/card0/gt_act_freq_mhz; sleep 0.1; done`
    
    Signed-off-by: Ed Baker <edward.baker@intel.com>
    Change-Id: I586134e4b52aea8b0422e18fb04b0de0103e9b57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2415037
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/gt/selftest_rps.c
+++ drivers/gpu/drm/i915/gt/selftest_rps.c
@@ -48,9 +48,9 @@ static int cmp_u64(const void *A, const void *B)
 {
 	const u64 *a = A, *b = B;
 
-	if (a < b)
+	if (*a < *b)
 		return -1;
-	else if (a > b)
+	else if (*a > *b)
 		return 1;
 	else
 		return 0;
@@ -60,9 +60,9 @@ static int cmp_u32(const void *A, const void *B)
 {
 	const u32 *a = A, *b = B;
 
-	if (a < b)
+	if (*a < *b)
 		return -1;
-	else if (a > b)
+	else if (*a > *b)
 		return 1;
 	else
 		return 0;
