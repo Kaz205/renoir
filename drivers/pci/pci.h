3c00e94c0b2ebe3b80f062ab0a4938789e4df7a8
Rajat Jain <rajatja@google.com>
2020-07-10 13:53:50 -0700

    CHROMIUM: PCI: Allowlist for drivers allowed to bind to untrusted devices
    Today, firmware can mark certain rootports as "external-facing" and the
    devices downstream those are treated as "untrusted". In chromeos, this
    mechanism is used to mark thunderbolt port and SD express ports as
    external-facing, an thus any external PCI devices further downstream as
    untrusted.
    
    This patch restricts the PCI drivers that are allowed to bind to those
    external PCI (untrusted) devices. The allowlist of drivers is to be fed
    by userspace via a sysfs attribute:
    
    /sys/bus/pci/drivers/drivers_allowlist
        When read, shows the current allowlist of drivers (one per line).
        When written, allows to add a driver to the allowlist.
    
    Note that adding drivers to allowlist only affects future binding of
    that driver (to external devices that are discovered in future).
    
    PS: The approach of having an allowlist for PCI drivers for external PCI
    devices was discussed at length upstream, but unfortunately nack'ed by
    upstream. The discussion was scattered across multiple (long) threads.
    https://lore.kernel.org/linux-pci/20200607113632.GA49147@kroah.com/t/#u
    https://lkml.org/lkml/2020/6/30/23
    https://lore.kernel.org/linux-pci/20200626002710.110200-2-rajatja@google.com/
    https://lore.kernel.org/linux-pci/20200616011742.138975-4-rajatja@google.com/T/#u
    
    BUG=b:153180503,b:151065827,b:140645321
    TEST=Test using the Caldigit docking station that the external PCI
    devices are attached to driver only when the driver entry is added
    to the allowlist.
    
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Change-Id: I0647e671c1c098dbe59b19883a1d78c37069161d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2298049
    Reviewed-by: Alex Levin <levinale@google.com>
    Reviewed-by: Prashant Malani <pmalani@chromium.org>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -687,4 +687,6 @@ static inline int pci_acpi_program_hp_params(struct pci_dev *dev)
 }
 #endif
 
+extern bool pci_drv_allowed_for_untrusted_devs(struct device_driver *drvr);
+
 #endif /* DRIVERS_PCI_H */
