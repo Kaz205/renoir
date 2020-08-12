a577409d72e72479572495e2b267ddbfdd1956f0
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2020-08-11 18:30:40 -0700

    mm/vmscan: protect the workingset on anonymous LRU
    In current implementation, newly created or swap-in anonymous page is
    started on active list.  Growing active list results in rebalancing
    active/inactive list so old pages on active list are demoted to inactive
    list.  Hence, the page on active list isn't protected at all.
    
    Following is an example of this situation.
    
    Assume that 50 hot pages on active list.  Numbers denote the number of
    pages on active/inactive list (active | inactive).
    
    1. 50 hot pages on active list
    50(h) | 0
    
    2. workload: 50 newly created (used-once) pages
    50(uo) | 50(h)
    
    3. workload: another 50 newly created (used-once) pages
    50(uo) | 50(uo), swap-out 50(h)
    
    This patch tries to fix this issue.  Like as file LRU, newly created or
    swap-in anonymous pages will be inserted to the inactive list.  They are
    promoted to active list if enough reference happens.  This simple
    modification changes the above example as following.
    
    1. 50 hot pages on active list
    50(h) | 0
    
    2. workload: 50 newly created (used-once) pages
    50(h) | 50(uo)
    
    3. workload: another 50 newly created (used-once) pages
    50(h) | 50(uo), swap-out 50(uo)
    
    As you can see, hot pages on active list would be protected.
    
    Note that, this implementation has a drawback that the page cannot be
    promoted and will be swapped-out if re-access interval is greater than the
    size of inactive list but less than the size of total(active+inactive).
    To solve this potential issue, following patch will apply workingset
    detection similar to the one that's already applied to file LRU.
    
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Link: http://lkml.kernel.org/r/1595490560-15117-3-git-send-email-iamjoonsoo.kim@lge.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1032,8 +1032,6 @@ static enum page_references page_check_references(struct page *page,
 		return PAGEREF_KEEP;
 
 	if (referenced_ptes) {
-		if (PageSwapBacked(page))
-			return PAGEREF_ACTIVATE;
 		/*
 		 * All mapped pages start out with page table
 		 * references from the instantiating fault, so we need
@@ -1056,7 +1054,7 @@ static enum page_references page_check_references(struct page *page,
 		/*
 		 * Activate file-backed executable pages after first usage.
 		 */
-		if (vm_flags & VM_EXEC)
+		if ((vm_flags & VM_EXEC) && !PageSwapBacked(page))
 			return PAGEREF_ACTIVATE;
 
 		return PAGEREF_KEEP;
