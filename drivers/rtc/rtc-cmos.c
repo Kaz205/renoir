e15396c705649fdf598464bf57cdce8c6f69c674
Victor Ding <victording@google.com>
2020-08-14 19:17:30 +1000

    FROMLIST: rtc: cmos: zero-init wkalrm when reading from CMOS
    cmos_read_alarm() may leave certain fields of a struct rtc_wkalrm
    untouched; therefore, these fields contain garbage if not properly
    initialized, leading to inconsistent values when converting into
    time64_t. This patch to zero initialize the struct before calling
    cmos_read_alarm().
    
    Note that this patch is not intended to produce a correct time64_t, it
    is only to produce a consistent value. In the case of suspend/resume, a
    correct time64_t is not necessary; a consistent value is sufficient to
    correctly perform an equality test for t_current_expires and
    t_saved_expires. Logic to deduce a correct time64_t is expensive and
    hence should be avoided.
    
    Signed-off-by: Victor Ding <victording@google.com>
    (am from https://lore.kernel.org/patchwork/patch/1287356/)
    (also found at https://lore.kernel.org/r/20200814191654.v2.1.Iaf7638a2f2a87ff68d85fcb8dec615e41340c97f@changeid)
    
    BUG=b:160277722
    TEST=Build Success
    
    Change-Id: Iaf7638a2f2a87ff68d85fcb8dec615e41340c97f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2390617
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>
    Tested-by: Victor Ding <victording@chromium.org>
    Auto-Submit: Victor Ding <victording@chromium.org>
    Commit-Queue: Victor Ding <victording@chromium.org>

--- drivers/rtc/rtc-cmos.c
+++ drivers/rtc/rtc-cmos.c
@@ -1005,6 +1005,7 @@ static int cmos_suspend(struct device *dev)
 			enable_irq_wake(cmos->irq);
 	}
 
+	memset(&cmos->saved_wkalrm, 0, sizeof(struct rtc_wkalrm));
 	cmos_read_alarm(dev, &cmos->saved_wkalrm);
 
 	dev_dbg(dev, "suspend%s, ctrl %02x\n",
@@ -1053,6 +1054,7 @@ static void cmos_check_wkalrm(struct device *dev)
 		return;
 	}
 
+	memset(&current_alarm, 0, sizeof(struct rtc_wkalrm));
 	cmos_read_alarm(dev, &current_alarm);
 	t_current_expires = rtc_tm_to_time64(&current_alarm.time);
 	t_saved_expires = rtc_tm_to_time64(&cmos->saved_wkalrm.time);
