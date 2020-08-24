ea49fd0d97ef1077f9a5b4fcf641c6024a4aa045
Mika Westerberg <mika.westerberg@linux.intel.com>
2020-08-24 12:46:52 +0300

    UPSTREAM: thunderbolt: Allow KUnit tests to be built also when CONFIG_USB4=m
    This adds a bit more build coverage for the tests even though these are
    not expected to be enabled by normal users and distros. In order to make
    this working we need to open-code kunit_test_suite() and call the
    relevant functions directly in the driver init/exit hook.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    (cherry picked from commit 2c6ea4e2cefe2e86af782a5b8e1070f4d434f2f2)
    
    BUG=b:156447982
    TEST=None
    
    Signed-off-by: Casey Bowman <casey.g.bowman@intel.com>
    Change-Id: Ief1036f2e9f5b320e4b4baff97b8e9eed290e556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2540767
    Reviewed-by: Prashant Malani <pmalani@chromium.org>
    Tested-by: Hou-hsun Lee <hou-hsun.lee@intel.corp-partner.google.com>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>

--- drivers/thunderbolt/domain.c
+++ drivers/thunderbolt/domain.c
@@ -800,6 +800,8 @@ int tb_domain_init(void)
 {
 	int ret;
 
+	tb_test_init();
+
 	tb_debugfs_init();
 	ret = tb_xdomain_init();
 	if (ret)
@@ -814,6 +816,7 @@ err_xdomain:
 	tb_xdomain_exit();
 err_debugfs:
 	tb_debugfs_exit();
+	tb_test_exit();
 
 	return ret;
 }
@@ -825,4 +828,5 @@ void tb_domain_exit(void)
 	tb_nvm_exit();
 	tb_xdomain_exit();
 	tb_debugfs_exit();
+	tb_test_exit();
 }
