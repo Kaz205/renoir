9b8e5f5ba5dbb92bd637f0d43c1763c68d211150
Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
2020-09-13 02:46:34 +0000

    UPSTREAM: ALSA: hda: avoid reset of sdo_limit
    By default 'sdo_limit' is initialized with a default value of '8'
    as per spec. This is overridden in cases where a different value is
    required. However this is getting reset when snd_hdac_bus_init_chip()
    is called again, which happens during runtime PM cycle.
    
    Avoid this reset by moving 'sdo_limit' setup to 'snd_hdac_bus_init()'
    function which would be called only once.
    
    Fixes: 67ae482a59e9 ("ALSA: hda: add member to store ratio for stripe control")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Sameer Pujar <spujar@nvidia.com>
    Link: https://lore.kernel.org/r/1597851130-6765-1-git-send-email-spujar@nvidia.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit b90b925fd52c75ee7531df739d850a1f7c58ef06)
    
    BUG=b:162008784
    TEST=Test audio on Volteer
    
    Signed-off-by: Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Change-Id: I99e09053f8447b8fa98e4848402e9b146d4feaef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2408348
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>

--- sound/hda/hdac_bus.c
+++ sound/hda/hdac_bus.c
@@ -44,6 +44,18 @@ int snd_hdac_bus_init(struct hdac_bus *bus, struct device *dev,
 	mutex_init(&bus->lock);
 	INIT_LIST_HEAD(&bus->hlink_list);
 	bus->irq = -1;
+
+	/*
+	 * Default value of '8' is as per the HD audio specification (Rev 1.0a).
+	 * Following relation is used to derive STRIPE control value.
+	 *  For sample rate <= 48K:
+	 *   { ((num_channels * bits_per_sample) / number of SDOs) >= 8 }
+	 *  For sample rate > 48K:
+	 *   { ((num_channels * bits_per_sample * rate/48000) /
+	 *	number of SDOs) >= 8 }
+	 */
+	bus->sdo_limit = 8;
+
 	return 0;
 }
 EXPORT_SYMBOL_GPL(snd_hdac_bus_init);
