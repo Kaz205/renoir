a4729d8227437b9168282758eb66475f3853b9ea
Balakrishna Godavarthi <bgodavar@codeaurora.org>
2020-10-06 11:59:27 +0530

    FROMLIST: Bluetooth: hci_qca: Enhance retry logic in qca_setup
    Currently driver only retries to download FW if FW downloading
    is failed. Sometimes observed command timeout for version request
    command, if this happen on some platforms during boot time, then
    a reboot is needed to turn ON BT. Instead to avoid a reboot, now
    extended retry logic for version request command too.
    
    Signed-off-by: Balakrishna Godavarthi <bgodavar@codeaurora.org>
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    (am from https://patchwork.kernel.org/patch/11818085/)
    (also found at https://lore.kernel.org/r/1601965767-18796-1-git-send-email-bgodavar@codeaurora.org)
    
    BUG=b:169427923
    TEST=Ran bluetooth_QuickSanity
    
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Change-Id: I7f15e0286a19802e30d89fa9655c4bdcb4f166e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2459907
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>
    Commit-Queue: Alain Michaud <alainm@chromium.org>

--- drivers/bluetooth/hci_qca.c
+++ drivers/bluetooth/hci_qca.c
@@ -1678,7 +1678,7 @@ static int qca_setup(struct hci_uart *hu)
 retry:
 	ret = qca_power_on(hdev);
 	if (ret)
-		return ret;
+		goto out;
 
 	clear_bit(QCA_SSR_TRIGGERED, &qca->flags);
 
@@ -1687,7 +1687,7 @@ retry:
 
 		ret = qca_read_soc_version(hdev, &soc_ver, soc_type);
 		if (ret)
-			return ret;
+			goto out;
 	} else {
 		qca_set_speed(hu, QCA_INIT_SPEED);
 	}
@@ -1697,7 +1697,7 @@ retry:
 	if (speed) {
 		ret = qca_set_speed(hu, QCA_OPER_SPEED);
 		if (ret)
-			return ret;
+			goto out;
 
 		qca_baudrate = qca_get_baudrate_value(speed);
 	}
@@ -1706,7 +1706,7 @@ retry:
 		/* Get QCA version information */
 		ret = qca_read_soc_version(hdev, &soc_ver, soc_type);
 		if (ret)
-			return ret;
+			goto out;
 	}
 
 	bt_dev_info(hdev, "QCA controller version 0x%08x", soc_ver);
@@ -1727,20 +1727,22 @@ retry:
 		 * patch/nvm-config is found, so run with original fw/config.
 		 */
 		ret = 0;
-	} else {
-		if (retries < MAX_INIT_RETRIES) {
-			qca_power_shutdown(hu);
-			if (hu->serdev) {
-				serdev_device_close(hu->serdev);
-				ret = serdev_device_open(hu->serdev);
-				if (ret) {
-					bt_dev_err(hdev, "failed to open port");
-					return ret;
-				}
+	}
+
+out:
+	if (ret && retries < MAX_INIT_RETRIES) {
+		bt_dev_warn(hdev, "Retry BT power ON:%d", retries);
+		qca_power_shutdown(hu);
+		if (hu->serdev) {
+			serdev_device_close(hu->serdev);
+			ret = serdev_device_open(hu->serdev);
+			if (ret) {
+				bt_dev_err(hdev, "failed to open port");
+				return ret;
 			}
-			retries++;
-			goto retry;
 		}
+		retries++;
+		goto retry;
 	}
 
 	/* Setup bdaddr */
