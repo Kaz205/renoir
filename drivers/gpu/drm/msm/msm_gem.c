7567a1c568123adfa0120a53302b34cff23f3063
Rob Clark <robdclark@chromium.org>
2020-10-23 09:51:18 -0700

    FROMGIT: drm/msm: Remove obj->gpu
    It cannot be atomically updated with obj->active_count, and the only
    purpose is a useless WARN_ON() (which becomes a buggy WARN_ON() once
    retire_submits() is not serialized with incoming submits via
    struct_mutex)
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Kristian H. Kristensen <hoegsberg@google.com>
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    (cherry picked from commit fb1a1fcbacefd839503e94a66a467809ade27b2c
     https://gitlab.freedesktop.org/drm/msm.git msm-next)
    
    BUG=b:171940670
    TEST=Build and boot, test webgl, etc
    
    Change-Id: Iedfaf7237f08794bf3dfb2dddc15e82071786fce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2521423
    Reviewed-by: Kristian H. Kristensen <hoegsberg@chromium.org>
    Reviewed-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Commit-Queue: Rob Clark <robdclark@chromium.org>
    Tested-by: Rob Clark <robdclark@chromium.org>
    Auto-Submit: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/msm_gem.c
+++ drivers/gpu/drm/msm/msm_gem.c
@@ -775,7 +775,6 @@ void msm_gem_active_get(struct drm_gem_object *obj, struct msm_gpu *gpu)
 
 	if (!atomic_fetch_inc(&msm_obj->active_count)) {
 		mutex_lock(&priv->mm_lock);
-		msm_obj->gpu = gpu;
 		list_del_init(&msm_obj->mm_list);
 		list_add_tail(&msm_obj->mm_list, &gpu->active_list);
 		mutex_unlock(&priv->mm_lock);
@@ -791,7 +790,6 @@ void msm_gem_active_put(struct drm_gem_object *obj)
 
 	if (!atomic_dec_return(&msm_obj->active_count)) {
 		mutex_lock(&priv->mm_lock);
-		msm_obj->gpu = NULL;
 		list_del_init(&msm_obj->mm_list);
 		list_add_tail(&msm_obj->mm_list, &priv->inactive_list);
 		mutex_unlock(&priv->mm_lock);
