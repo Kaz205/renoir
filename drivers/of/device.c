2e05dab15943cd69ced5a36fe772fb4bf3976e0e
Geert Uytterhoeven <geert+renesas@glider.be>
2020-10-26 16:27:55 +0100

    UPSTREAM: dma-mapping: fix 32-bit overflow with CONFIG_ARM_LPAE=n
    On r8a7791/koelsch and shmobile_defconfig, PCIe probing fails with:
    
        rcar-pcie fe000000.pcie: Adjusted size 0x0 invalid
        rcar-pcie: probe of fe000000.pcie failed with error -22
    
    of_dma_get_range() returns the following map:
    
        cpu_start 0x40000000 dma_start 0x40000000 size 0x080000000 offset 0
        cpu_start 0x00000000 dma_start 0x00000000 size 0x100000000 offset 0
    
    If CONFIG_ARM_LPAE=n, dma_addr_t is 32-bit.  Hence when assigning
    r->dma_start + r->size to dma_end, this value will be truncated to
    32-bit, yielding zero when processing the second table entry.
    Consequently, both dma_start and dma_end will be zero, leading to a zero
    size.
    
    Fix this by changing the dma_start and dma_end variables from dma_addr_t
    to u64.
    
    Fixes: e0d072782c734d27 ("dma-mapping: introduce DMA range map, supplanting dma_pfn_offset")
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 48ab6d5d1f096d6fac5b59f94af0aa394115a001)
    
    BUG=b:187155802
    TEST=Build and boot
    
    Signed-off-by: Flora Fu <flora.fu@mediatek.com>
    Change-Id: I4cbdbce96e2c083b20e5c60e45f0df7b1922f1a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3216766
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>

--- drivers/of/device.c
+++ drivers/of/device.c
@@ -92,7 +92,7 @@ int of_dma_configure_id(struct device *dev, struct device_node *np,
 {
 	const struct iommu_ops *iommu;
 	const struct bus_dma_region *map = NULL;
-	dma_addr_t dma_start = 0;
+	u64 dma_start = 0;
 	u64 mask, end, size = 0;
 	bool coherent;
 	int ret;
@@ -108,10 +108,10 @@ int of_dma_configure_id(struct device *dev, struct device_node *np,
 			return ret == -ENODEV ? 0 : ret;
 	} else {
 		const struct bus_dma_region *r = map;
-		dma_addr_t dma_end = 0;
+		u64 dma_end = 0;
 
 		/* Determine the overall bounds of all DMA regions */
-		for (dma_start = ~(dma_addr_t)0; r->size; r++) {
+		for (dma_start = ~0ULL; r->size; r++) {
 			/* Take lower and upper limits */
 			if (r->dma_start < dma_start)
 				dma_start = r->dma_start;
