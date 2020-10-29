6724e31ea3cc4c1ecf83a769a19e5113e1cdd195
Ryder Lee <ryder.lee@mediatek.com>
2020-10-30 01:11:16 +0800

    UPSTREAM: mt76: mt7915: remove unused mt7915_mcu_bss_sync_tlv()
    mt7915_mcu_bss_sync_tlv() is no longer used, so remove it.
    
    BUG=b:178754244
    TEST=build and boot on asurada
    
    (cherry picked from commit 802145e2ed2f71e1a29f64d5873e9e71e3495676)
    Signed-off-by: Sean Wang <objelf@gmail.com>
    Signed-off-by: Ryder Lee <ryder.lee@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Change-Id: Ie8a1844bd126d0676ee5feb22aee4d20d0581b82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2680640
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Claire Chang <tientzu@chromium.org>
    Tested-by: Claire Chang <tientzu@chromium.org>
    Commit-Queue: Claire Chang <tientzu@chromium.org>

--- drivers/net/wireless/mediatek/mt76/mt7915/mcu.h
+++ drivers/net/wireless/mediatek/mt76/mt7915/mcu.h
@@ -382,15 +382,6 @@ struct bss_info_ext_bss {
 	u8 rsv[8];
 } __packed;
 
-struct bss_info_sync_mode {
-	__le16 tag;
-	__le16 len;
-	__le16 bcn_interval;
-	u8 enable;
-	u8 dtim_period;
-	u8 rsv[8];
-} __packed;
-
 struct bss_info_bmc_rate {
 	__le16 tag;
 	__le16 len;
@@ -508,7 +499,7 @@ enum {
 	BSS_INFO_LQ_RM,		/* obsoleted */
 	BSS_INFO_EXT_BSS,
 	BSS_INFO_BMC_RATE,	/* for bmc rate control in CR4 */
-	BSS_INFO_SYNC_MODE,
+	BSS_INFO_SYNC_MODE,	/* obsoleted */
 	BSS_INFO_RA,
 	BSS_INFO_HW_AMSDU,
 	BSS_INFO_BSS_COLOR,
@@ -1035,8 +1026,7 @@ enum {
 					 sizeof(struct bss_info_hw_amsdu) +\
 					 sizeof(struct bss_info_he) +	\
 					 sizeof(struct bss_info_bmc_rate) +\
-					 sizeof(struct bss_info_ext_bss) +\
-					 sizeof(struct bss_info_sync_mode))
+					 sizeof(struct bss_info_ext_bss))
 
 #define MT7915_BEACON_UPDATE_SIZE	(sizeof(struct sta_req_hdr) +	\
 					 sizeof(struct bss_info_bcn_csa) + \
