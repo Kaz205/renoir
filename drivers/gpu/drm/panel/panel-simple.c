2c3d9ab5c536a3aff3dbb4ce5adfa3654aa4c145
Douglas Anderson <dianders@chromium.org>
2020-11-05 13:57:39 -0800

    FROMLIST: drm: panel: simple: Allow timing constraints, not fixed delays
    The simple panel code currently allows panels to define fixed delays
    at certain stages of initialization.  These work OK, but they don't
    really map all that clearly to the requirements presented in many
    panel datasheets.  Instead of defining a fixed delay, those datasheets
    provide a timing diagram and specify a minimum amount of time that
    needs to pass from event A to event B.
    
    Because of the way things are currently defined, most panels end up
    over-delaying.  One prime example here is that a number of panels I've
    looked at define the amount of time that must pass between turning a
    panel off and turning it back on again.  Since there is no way to
    specify this, many developers have listed this as the "unprepare"
    delay.  However, if nobody ever tried to turn the panel on again in
    the next 500 ms (or whatever the delay was) then this delay was
    pointless.  It's better to do the delay only in the case that someone
    tried to turn the panel on too quickly.
    
    Let's support specifying delays as constraints.  We'll start with the
    one above and also a second one: the minimum time between prepare
    being done and doing the enable.  On the panel I'm looking at, there's
    an 80 ms minimum time between HPD being asserted by the panel and
    setting the backlight enable GPIO.  By specifying as a constraint we
    can enforce this without over-delaying.  Specifically the link
    training is allowed to happen in parallel with this delay so adding a
    fixed 80 ms delay isn't ideal.
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    (am from https://lore.kernel.org/patchwork/patch/1333682/)
    (also found at https://lore.kernel.org/r/20201105135639.v3.1.I31c4f8b111dbef1ab658f206764655ae983bc560@changeid)
    
    BUG=b:169288627, b:171536503
    TEST=Boot coachz
    
    Change-Id: I31c4f8b111dbef1ab658f206764655ae983bc560
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2495746
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Rob Clark <robdclark@chromium.org>
    Tested-by: Douglas Anderson <dianders@chromium.org>
    Commit-Queue: Douglas Anderson <dianders@chromium.org>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -92,6 +92,68 @@ struct panel_desc {
 		unsigned int unprepare;
 	} delay;
 
+	struct {
+		/**
+		 * @prepare_to_enable: Time between prepare and enable.
+		 *
+		 * The minimum time, in milliseconds, that needs to have passed
+		 * between when prepare finished and enable may begin. If at
+		 * enable time less time has passed since prepare finished,
+		 * the driver waits for the remaining time.
+		 *
+		 * If a fixed enable delay is also specified, we'll start
+		 * counting before delaying for the fixed delay.
+		 *
+		 * If a fixed prepare delay is also specified, we won't start
+		 * counting until after the fixed delay. We can't overlap this
+		 * fixed delay with the min time because the fixed delay
+		 * doesn't happen at the end of the function if a HPD GPIO was
+		 * specified.
+		 *
+		 * In other words:
+		 *   prepare()
+		 *     ...
+		 *     // do fixed prepare delay
+		 *     // wait for HPD GPIO if applicable
+		 *     // start counting for prepare_to_enable
+		 *
+		 *   enable()
+		 *     // do fixed enable delay
+		 *     // enforce prepare_to_enable min time
+		 */
+		unsigned int prepare_to_enable;
+
+		/**
+		 * @unprepare_to_prepare: Time between unprepare and prepare.
+		 *
+		 * The minimum time, in milliseconds, that needs to have passed
+		 * between when unprepare finished and prepare may begin. If at
+		 * prepare time less time has passed since unprepare finished,
+		 * the driver waits for the remaining time.
+		 *
+		 * If a fixed unprepare delay is also specified, we'll start
+		 * counting before delaying for the fixed delay.
+		 *
+		 * If a fixed prepare delay is also specified, it will happen
+		 * separately and after we've enforced this minimum. We can't
+		 * overlap this fixed delay with the min time because the
+		 * fixed delay doesn't happen at the start of the function
+		 * if a regulator or enable GPIO was specified.
+		 *
+		 * In other words:
+		 *   unprepare():
+		 *     ...
+		 *     // start counting for unprepare_to_prepare
+		 *     // do fixed unprepare delay
+		 *
+		 *   prepare():
+		 *     // enforce unprepare_to_prepare min time
+		 *     // turn on regulator / set enable GPIO if applicable
+		 *     // do fixed prepare delay
+		 */
+		unsigned int unprepare_to_prepare;
+	} min_times;
+
 	u32 bus_format;
 	u32 bus_flags;
 	int connector_type;
@@ -99,9 +161,11 @@ struct panel_desc {
 
 struct panel_simple {
 	struct drm_panel base;
-	bool prepared;
 	bool enabled;
 	bool no_hpd;
+
+	ktime_t prepared_time;
+	ktime_t unprepared_time;
 
 	const struct panel_desc *desc;
 
@@ -231,6 +295,20 @@ static int panel_simple_get_non_edid_modes(struct panel_simple *panel,
 	return num;
 }
 
+static void panel_simple_wait_min_time(ktime_t start_ktime, unsigned int min_ms)
+{
+	ktime_t now_ktime, min_ktime;
+
+	if (!min_ms)
+		return;
+
+	min_ktime = ktime_add(start_ktime, ms_to_ktime(min_ms));
+	now_ktime = ktime_get();
+
+	if (ktime_before(now_ktime, min_ktime))
+		msleep(ktime_to_ms(ktime_sub(min_ktime, now_ktime)) + 1);
+}
+
 static int panel_simple_disable(struct drm_panel *panel)
 {
 	struct panel_simple *p = to_panel_simple(panel);
@@ -250,17 +328,18 @@ static int panel_simple_unprepare(struct drm_panel *panel)
 {
 	struct panel_simple *p = to_panel_simple(panel);
 
-	if (!p->prepared)
+	if (p->prepared_time == 0)
 		return 0;
 
 	gpiod_set_value_cansleep(p->enable_gpio, 0);
 
 	regulator_disable(p->supply);
 
+	p->prepared_time = 0;
+	p->unprepared_time = ktime_get();
+
 	if (p->desc->delay.unprepare)
 		msleep(p->desc->delay.unprepare);
-
-	p->prepared = false;
 
 	return 0;
 }
@@ -297,8 +376,11 @@ static int panel_simple_prepare(struct drm_panel *panel)
 	int err;
 	int hpd_asserted;
 
-	if (p->prepared)
+	if (p->prepared_time != 0)
 		return 0;
+
+	panel_simple_wait_min_time(p->unprepared_time,
+				   p->desc->min_times.unprepare_to_prepare);
 
 	err = regulator_enable(p->supply);
 	if (err < 0) {
@@ -334,7 +416,7 @@ static int panel_simple_prepare(struct drm_panel *panel)
 		}
 	}
 
-	p->prepared = true;
+	p->prepared_time = ktime_get();
 
 	return 0;
 }
@@ -348,6 +430,9 @@ static int panel_simple_enable(struct drm_panel *panel)
 
 	if (p->desc->delay.enable)
 		msleep(p->desc->delay.enable);
+
+	panel_simple_wait_min_time(p->prepared_time,
+				   p->desc->min_times.prepare_to_enable);
 
 	p->enabled = true;
 
@@ -465,7 +550,7 @@ static int panel_simple_probe(struct device *dev, const struct panel_desc *desc)
 		return -ENOMEM;
 
 	panel->enabled = false;
-	panel->prepared = false;
+	panel->prepared_time = 0;
 	panel->desc = desc;
 
 	panel->no_hpd = of_property_read_bool(dev->of_node, "no-hpd");
