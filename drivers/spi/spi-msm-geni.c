f319945e80a3d157bd62dc22edcdb9366e170a23
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-05 17:17:02 -0800

    spi: spi-msm-geni: Add a function to get the IRQ of a device's master
    Exporting the IRQ of a SPI device's master controller can help device
    drivers utilize the PM QoS API to force the SPI master IRQ to be
    serviced with low latency.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [Kazuki: Port to v5.4]
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/spi/spi-msm-geni.c
+++ drivers/spi/spi-msm-geni.c
@@ -181,6 +181,13 @@ struct spi_geni_master {
 	bool use_fixed_timeout;
 };
 
+int geni_spi_get_master_irq(struct spi_device *spi_slv)
+{
+	struct spi_geni_master *mas = spi_master_get_devdata(spi_slv->master);
+
+	return mas->irq;
+}
+
 static void spi_slv_setup(struct spi_geni_master *mas);
 
 static ssize_t spi_slave_state_show(struct device *dev,
