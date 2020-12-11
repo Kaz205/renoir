f5a74350e49c7dff7d7fcf1b6514b4caceb76cad
Huang, Sean Z <sean.z.huang@intel.com>
2020-12-11 01:04:36 -0800

    FROMLIST: drm/i915/pxp: set KCR reg init during the boot time
    Set the KCR init during the boot time, which is
    required by hardware, to allow us doing further
    protection operation such as sending commands to
    GPU or TEE.
    
    Signed-off-by: Huang, Sean Z <sean.z.huang@intel.com>
    (am from https://patchwork.freedesktop.org/patch/407033/)
    
    BUG=b:155511178
    TEST=None
    
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Change-Id: I65fdf737a9d231447497f41bff9c5e72a37e08f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2587328
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/pxp/intel_pxp.c
+++ drivers/gpu/drm/i915/pxp/intel_pxp.c
@@ -6,6 +6,12 @@
 #include "intel_pxp.h"
 #include "intel_pxp_context.h"
 
+/* KCR register definitions */
+#define KCR_INIT            _MMIO(0x320f0)
+#define KCR_INIT_MASK_SHIFT (16)
+/* Setting KCR Init bit is required after system boot */
+#define KCR_INIT_ALLOW_DISPLAY_ME_WRITES (BIT(14) | (BIT(14) << KCR_INIT_MASK_SHIFT))
+
 int intel_pxp_init(struct intel_pxp *pxp)
 {
 	struct intel_gt *gt = container_of(pxp, struct intel_gt, pxp);
@@ -15,6 +21,8 @@ int intel_pxp_init(struct intel_pxp *pxp)
 		return -ENODEV;
 
 	intel_pxp_ctx_init(&pxp->ctx);
+
+	intel_uncore_write(gt->uncore, KCR_INIT, KCR_INIT_ALLOW_DISPLAY_ME_WRITES);
 
 	drm_info(&gt->i915->drm, "Protected Xe Path (PXP) protected content support initialized\n");
 
