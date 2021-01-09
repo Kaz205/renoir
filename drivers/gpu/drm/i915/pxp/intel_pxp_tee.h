a1f83a7359ec47eb1a51b0b6796401ee44eada42
Juston Li <juston.li@intel.com>
2021-01-08 17:42:33 -0800

    CHROMIUM: drm/i915/pxp: fix TEE command for creating arb session
    Turns out the arb session wasn't actually starting due to a version
    mismatch in the TEE command.
    
    Signed-off-by: Juston Li <juston.li@intel.com>
    
    BUG=none
    TEST='intel_reg read 0x32260' should be 0x8000 on boot
    
    Change-Id: I6bb51caf51a90e03ce66a4afb3c02d2be639f3f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2619038
    Tested-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Tested-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>

--- drivers/gpu/drm/i915/pxp/intel_pxp_tee.h
+++ drivers/gpu/drm/i915/pxp/intel_pxp_tee.h
@@ -19,7 +19,7 @@ int intel_pxp_tee_ioctl_io_message(struct intel_pxp *pxp,
 				   u32 msg_out_buf_size);
 
 /* TEE command to create the arbitrary session */
-#define PXP_TEE_ARB_CMD_BIN {0x00040000, 0x0000001e, 0x00000000, 0x00000008, 0x00000002, 0x0000000f}
+#define PXP_TEE_ARB_CMD_BIN {0x00040002, 0x0000001e, 0x00000000, 0x00000008, 0x00000002, 0x0000000f}
 #define PXP_TEE_ARB_CMD_DW_LEN (6)
 
 #endif /* __INTEL_PXP_TEE_H__ */
