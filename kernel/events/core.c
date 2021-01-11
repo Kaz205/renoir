ea85bfb32bef5f80f7befd48f69dc0dd26e7efe3
Greg Kroah-Hartman <gregkh@google.com>
2021-01-11 14:29:58 +0100

    Merge 5.4.88 into android11-5.4-lts
    Changes in 5.4.88
    	Revert "drm/amd/display: Fix memory leaks in S3 resume"
    	Revert "mtd: spinand: Fix OOB read"
    	dmaengine: at_hdmac: Substitute kzalloc with kmalloc
    	dmaengine: at_hdmac: add missing put_device() call in at_dma_xlate()
    	dmaengine: at_hdmac: add missing kfree() call in at_dma_xlate()
    	kdev_t: always inline major/minor helper functions
    	iio:imu:bmi160: Fix alignment and data leak issues
    	fuse: fix bad inode
    	perf: Break deadlock involving exec_update_mutex
    	rwsem: Implement down_read_killable_nested
    	rwsem: Implement down_read_interruptible
    	exec: Transform exec_update_mutex into a rw_semaphore
    	mwifiex: Fix possible buffer overflows in mwifiex_cmd_802_11_ad_hoc_start
    	Linux 5.4.88
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I2e7792fc0001b7df699b4477fabf97914c0aeceb

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1247,7 +1247,7 @@ static void put_ctx(struct perf_event_context *ctx)
  * function.
  *
  * Lock order:
- *    exec_update_mutex
+ *    exec_update_lock
  *	task_struct::perf_event_mutex
  *	  perf_event_context::mutex
  *	    perf_event::child_mutex;
@@ -11029,24 +11029,6 @@ SYSCALL_DEFINE5(perf_event_open,
 		goto err_task;
 	}
 
-	if (task) {
-		err = mutex_lock_interruptible(&task->signal->exec_update_mutex);
-		if (err)
-			goto err_task;
-
-		/*
-		 * Reuse ptrace permission checks for now.
-		 *
-		 * We must hold exec_update_mutex across this and any potential
-		 * perf_install_in_context() call for this new event to
-		 * serialize against exec() altering our credentials (and the
-		 * perf_event_exit_task() that could imply).
-		 */
-		err = -EACCES;
-		if (!ptrace_may_access(task, PTRACE_MODE_READ_REALCREDS))
-			goto err_cred;
-	}
-
 	if (flags & PERF_FLAG_PID_CGROUP)
 		cgroup_fd = pid;
 
@@ -11054,7 +11036,7 @@ SYSCALL_DEFINE5(perf_event_open,
 				 NULL, NULL, cgroup_fd);
 	if (IS_ERR(event)) {
 		err = PTR_ERR(event);
-		goto err_cred;
+		goto err_task;
 	}
 
 	if (is_sampling_event(event)) {
@@ -11171,6 +11153,24 @@ SYSCALL_DEFINE5(perf_event_open,
 		err = PTR_ERR(event_file);
 		event_file = NULL;
 		goto err_context;
+	}
+
+	if (task) {
+		err = down_read_interruptible(&task->signal->exec_update_lock);
+		if (err)
+			goto err_file;
+
+		/*
+		 * Preserve ptrace permission check for backwards compatibility.
+		 *
+		 * We must hold exec_update_lock across this and any potential
+		 * perf_install_in_context() call for this new event to
+		 * serialize against exec() altering our credentials (and the
+		 * perf_event_exit_task() that could imply).
+		 */
+		err = -EACCES;
+		if (!ptrace_may_access(task, PTRACE_MODE_READ_REALCREDS))
+			goto err_cred;
 	}
 
 	if (move_group) {
@@ -11326,7 +11326,7 @@ SYSCALL_DEFINE5(perf_event_open,
 	mutex_unlock(&ctx->mutex);
 
 	if (task) {
-		mutex_unlock(&task->signal->exec_update_mutex);
+		up_read(&task->signal->exec_update_lock);
 		put_task_struct(task);
 	}
 
@@ -11348,7 +11348,10 @@ err_locked:
 	if (move_group)
 		perf_event_ctx_unlock(group_leader, gctx);
 	mutex_unlock(&ctx->mutex);
-/* err_file: */
+err_cred:
+	if (task)
+		up_read(&task->signal->exec_update_lock);
+err_file:
 	fput(event_file);
 err_context:
 	perf_unpin_context(ctx);
@@ -11360,9 +11363,6 @@ err_alloc:
 	 */
 	if (!event_file)
 		free_event(event);
-err_cred:
-	if (task)
-		mutex_unlock(&task->signal->exec_update_mutex);
 err_task:
 	if (task)
 		put_task_struct(task);
@@ -11667,7 +11667,7 @@ static void perf_event_exit_task_context(struct task_struct *child, int ctxn)
 /*
  * When a child task exits, feed back event values to parent events.
  *
- * Can be called with exec_update_mutex held when called from
+ * Can be called with exec_update_lock held when called from
  * install_exec_creds().
  */
 void perf_event_exit_task(struct task_struct *child)
