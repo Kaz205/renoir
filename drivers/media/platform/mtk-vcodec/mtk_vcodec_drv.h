e18a7c68d63f8096ef5b9eb35930275a93d8191c
Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
2021-01-11 11:15:00 +0800

    CHROMIUM: media: mtk-vcodec: Add msg queue feature for lat and core architecture
    For lat and core architecture, lat thread will send message to core
    thread when lat decode done. Core hardware will use the message
    from lat to decode, then free message to lat thread when decode done.
    
    BUG=b:167469726
    BUG=b:167466842
    BUG=b:185094057
    TEST=VDAtest passes on Asurada
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Change-Id: I492bf708384a70da5442962d0d800893a2d4296d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2614324
    Reviewed-by: Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Tested-by: Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
    Tested-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_drv.h
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_drv.h
@@ -17,7 +17,9 @@
 #include <media/v4l2-ioctl.h>
 #include <media/v4l2-mem2mem.h>
 #include <media/videobuf2-core.h>
+
 #include "mtk_vcodec_util.h"
+#include "vdec_msg_queue.h"
 
 #define MTK_VCODEC_DRV_NAME	"mtk_vcodec_drv"
 #define MTK_VCODEC_DEC_NAME	"mtk-vcodec-dec"
@@ -308,6 +310,7 @@ struct mtk_vcodec_ctx {
 	int decoded_frame_cnt;
 	struct mutex lock;
 
+	struct vdec_msg_queue msg_queue;
 };
 
 enum mtk_chip {
@@ -479,6 +482,11 @@ struct mtk_vcodec_dev {
 	struct mtk_vcodec_dev *hw_dev[MTK_VDEC_HW_MAX];
 	struct device_node *component_node[MTK_VDEC_HW_MAX];
 	int comp_idx;
+
+	wait_queue_head_t core_read;
+	struct list_head core_queue;
+	spinlock_t core_lock;
+	int num_core;
 };
 
 static inline struct mtk_vcodec_ctx *fh_to_ctx(struct v4l2_fh *fh)
