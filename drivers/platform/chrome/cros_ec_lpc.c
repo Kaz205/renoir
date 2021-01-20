98e7ad6040ee883aa88386705221272768a04282
Prashant Malani <pmalani@chromium.org>
2021-01-20 14:42:01 -0800

    Revert "CHROMIUM: platform/chrome: lpc: Add extcon support"
    This reverts commit 3cda3f6a543de1342a08c5204737ef7c8dcfc28f.
    
    BUG=b:168705865
    TEST=Build and boot on a voxel. Ensure that Type C functionality is
    unaffected (thunderbolt hotplug verified with a Dell WD19TB dock).
    
    Change-Id: I1d9e796f370c367bb13a79b05dde62757ca880fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2641170
    Tested-by: Prashant Malani <pmalani@chromium.org>
    Reviewed-by: Alex Levin <levinale@google.com>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>

--- drivers/platform/chrome/cros_ec_lpc.c
+++ drivers/platform/chrome/cros_ec_lpc.c
@@ -428,9 +428,7 @@ static int cros_ec_lpc_probe(struct platform_device *pdev)
 	}
 
 	/* Revert this after we introduce Type C connector class driver. */
-	if (dmi_match(DMI_PRODUCT_NAME, "Volteer") ||
-	    dmi_match(DMI_PRODUCT_NAME, "Delbin") ||
-	    dmi_match(DMI_PRODUCT_NAME, "tglrvp"))
+	if (dmi_match(DMI_PRODUCT_NAME, "Volteer"))
 		pdev_extcon = platform_device_register_data(dev,
 					"extcon-tcss-cros-ec",
 					PLATFORM_DEVID_NONE, NULL, 0);
