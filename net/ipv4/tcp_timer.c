f6bf1a0fb8be241a7049f984e129d975ebdd43df
Greg Kroah-Hartman <gregkh@google.com>
2021-02-07 16:03:32 +0100

    Merge 5.4.96 into android11-5.4-lts
    Changes in 5.4.96
    	net: dsa: bcm_sf2: put device node before return
    	net: switchdev: don't set port_obj_info->handled true when -EOPNOTSUPP
    	ibmvnic: Ensure that CRQ entry read are correctly ordered
    	Revert "Revert "block: end bio with BLK_STS_AGAIN in case of non-mq devs and REQ_NOWAIT""
    	ACPI: thermal: Do not call acpi_thermal_check() directly
    	arm64: Fix kernel address detection of __is_lm_address()
    	arm64: Do not pass tagged addresses to __is_lm_address()
    	tcp: make TCP_USER_TIMEOUT accurate for zero window probes
    	btrfs: backref, only collect file extent items matching backref offset
    	btrfs: backref, don't add refs from shared block when resolving normal backref
    	btrfs: backref, only search backref entries from leaves of the same root
    	btrfs: backref, use correct count to resolve normal data refs
    	net_sched: gen_estimator: support large ewma log
    	phy: cpcap-usb: Fix warning for missing regulator_disable
    	platform/x86: touchscreen_dmi: Add swap-x-y quirk for Goodix touchscreen on Estar Beauty HD tablet
    	platform/x86: intel-vbtn: Support for tablet mode on Dell Inspiron 7352
    	x86: __always_inline __{rd,wr}msr()
    	scsi: scsi_transport_srp: Don't block target in failfast state
    	scsi: libfc: Avoid invoking response handler twice if ep is already completed
    	scsi: fnic: Fix memleak in vnic_dev_init_devcmd2
    	ASoC: SOF: Intel: hda: Resume codec to do jack detection
    	mac80211: fix fast-rx encryption check
    	scsi: ibmvfc: Set default timeout to avoid crash during migration
    	ALSA: hda: Add Cometlake-R PCI ID
    	udf: fix the problem that the disc content is not displayed
    	nvme: check the PRINFO bit before deciding the host buffer length
    	selftests/powerpc: Only test lwm/stmw on big endian
    	drm/amd/display: Update dram_clock_change_latency for DCN2.1
    	drm/amd/display: Change function decide_dp_link_settings to avoid infinite looping
    	objtool: Don't fail on missing symbol table
    	kthread: Extract KTHREAD_IS_PER_CPU
    	workqueue: Restrict affinity change to rescuer
    	Linux 5.4.96
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ib235d179dcd1050a5268dd9a3b61db62a6cb9889
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>
    
    Conflicts:
    	net/ipv4/tcp_timer.c

--- net/ipv4/tcp_timer.c
+++ net/ipv4/tcp_timer.c
@@ -73,6 +73,24 @@ int tcp_use_userconfig_sysctl_handler(struct ctl_table *table, int write,
 	return ret;
 }
 
+u32 tcp_clamp_probe0_to_user_timeout(const struct sock *sk, u32 when)
+{
+	struct inet_connection_sock *icsk = inet_csk(sk);
+	u32 remaining;
+	s32 elapsed;
+
+	if (!icsk->icsk_user_timeout || !icsk->icsk_probes_tstamp)
+		return when;
+
+	elapsed = tcp_jiffies32 - icsk->icsk_probes_tstamp;
+	if (unlikely(elapsed < 0))
+		elapsed = 0;
+	remaining = msecs_to_jiffies(icsk->icsk_user_timeout) - elapsed;
+	remaining = max_t(u32, remaining, TCP_TIMEOUT_MIN);
+
+	return min_t(u32, remaining, when);
+}
+
 /**
  *  tcp_write_err() - close socket and save error info
  *  @sk:  The socket the error has appeared on.
