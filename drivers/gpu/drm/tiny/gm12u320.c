476bc4b456eb471795b349eac7da3dbaaa5b601d
Thomas Zimmermann <tzimmermann@suse.de>
2021-03-03 14:32:29 +0100

    BACKPORT: drm: Use USB controller's DMA mask when importing dmabufs
    USB devices cannot perform DMA and hence have no dma_mask set in their
    device structure. Therefore importing dmabuf into a USB-based driver
    fails, which breaks joining and mirroring of display in X11.
    
    For USB devices, pick the associated USB controller as attachment device.
    This allows the DRM import helpers to perform the DMA setup. If the DMA
    controller does not support DMA transfers, we're out of luck and cannot
    import. Our current USB-based DRM drivers don't use DMA, so the actual
    DMA device is not important.
    
    Tested by joining/mirroring displays of udl and radeon under Gnome/X11.
    
    v8:
    	* release dmadev if device initialization fails (Noralf)
    	* fix commit description (Noralf)
    v7:
    	* fix use-before-init bug in gm12u320 (Dan)
    v6:
    	* implement workaround in DRM drivers and hold reference to
    	  DMA device while USB device is in use
    	* remove dev_is_usb() (Greg)
    	* collapse USB helper into usb_intf_get_dma_device() (Alan)
    	* integrate Daniel's TODO statement (Daniel)
    	* fix typos (Greg)
    v5:
    	* provide a helper for USB interfaces (Alan)
    	* add FIXME item to documentation and TODO list (Daniel)
    v4:
    	* implement workaround with USB helper functions (Greg)
    	* use struct usb_device->bus->sysdev as DMA device (Takashi)
    v3:
    	* drop gem_create_object
    	* use DMA mask of USB controller, if any (Daniel, Christian, Noralf)
    v2:
    	* move fix to importer side (Christian, Daniel)
    	* update SHMEM and CMA helpers for new PRIME callbacks
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Fixes: 6eb0233ec2d0 ("usb: don't inherity DMA properties for USB devices")
    Tested-by: Pavel Machek <pavel@ucw.cz>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Christian König <christian.koenig@amd.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Noralf Trønnes <noralf@tronnes.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: <stable@vger.kernel.org> # v5.10+
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210303133229.3288-1-tzimmermann@suse.de
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    (cherry picked from commit 659ab7a49cbebe0deffcbe1f9560e82006b21817)
    
    Conflicts:
    	Documentation/gpu/todo.rst
    	drivers/gpu/drm/tiny/gm12u320.c
    	drivers/gpu/drm/udl/udl_drv.c
    	drivers/gpu/drm/udl/udl_drv.h
    	drivers/gpu/drm/udl/udl_main.c
    (1. fix line offset conflict in Documentation/gpu/todo.rst
     2. drivers/gpu/drm/tiny/gm12u320.c
        different context in gm12u320.c. Add the patch but not change the old code.
     3. drivers/gpu/drm/udl/udl_drv.c
        drivers/gpu/drm/udl/udl_drv.h
        drivers/gpu/drm/udl/udl_main.c
        udl driver's intenal drm_driver driver is different context.
        Add the dmadev for the usb interface's DMA endpoint but drop
        import buffer patch in udl_drv.c due to the driver context is legacy in v5.4.)
    
    BUG=b:187155802
    TEST=Build and boot
    
    Signed-off-by: Flora Fu <flora.fu@mediatek.com>
    Change-Id: Ic3ebaccbf564498e2b859eb4d524a85c6906d457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3216768
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>

--- drivers/gpu/drm/tiny/gm12u320.c
+++ drivers/gpu/drm/tiny/gm12u320.c
@@ -83,6 +83,7 @@ MODULE_PARM_DESC(eco_mode, "Turn on Eco mode (less bright, more silent)");
 
 struct gm12u320_device {
 	struct drm_device	         dev;
+	struct device                   *dmadev;
 	struct drm_simple_display_pipe   pipe;
 	struct drm_connector	         conn;
 	struct usb_device               *udev;
@@ -651,6 +652,22 @@ static void gm12u320_driver_release(struct drm_device *dev)
 	kfree(gm12u320);
 }
 
+/*
+ * FIXME: Dma-buf sharing requires DMA support by the importing device.
+ *        This function is a workaround to make USB devices work as well.
+ *        See todo.rst for how to fix the issue in the dma-buf framework.
+ */
+static struct drm_gem_object *gm12u320_gem_prime_import(struct drm_device *dev,
+							struct dma_buf *dma_buf)
+{
+	struct gm12u320_device *gm12u320 = to_gm12u320(dev);
+
+	if (!gm12u320->dmadev)
+		return ERR_PTR(-ENODEV);
+
+	return drm_gem_prime_import_dev(dev, dma_buf, gm12u320->dmadev);
+}
+
 DEFINE_DRM_GEM_SHMEM_FOPS(gm12u320_fops);
 
 static struct drm_driver gm12u320_drm_driver = {
@@ -665,6 +682,7 @@ static struct drm_driver gm12u320_drm_driver = {
 	.release	 = gm12u320_driver_release,
 	.fops		 = &gm12u320_fops,
 	DRM_GEM_SHMEM_DRIVER_OPS,
+	.gem_prime_import = gm12u320_gem_prime_import,
 };
 
 static const struct drm_mode_config_funcs gm12u320_mode_config_funcs = {
@@ -690,15 +708,20 @@ static int gm12u320_usb_probe(struct usb_interface *interface,
 	gm12u320 = kzalloc(sizeof(*gm12u320), GFP_KERNEL);
 	if (gm12u320 == NULL)
 		return -ENOMEM;
+	dev = &gm12u320->dev;
+
+	gm12u320->dmadev = usb_intf_get_dma_device(to_usb_interface(dev->dev));
+	if (!gm12u320->dmadev)
+		drm_warn(dev, "buffer sharing not supported"); /* not an error */
 
 	gm12u320->udev = interface_to_usbdev(interface);
 	INIT_WORK(&gm12u320->fb_update.work, gm12u320_fb_update_work);
 	mutex_init(&gm12u320->fb_update.lock);
 	init_waitqueue_head(&gm12u320->fb_update.waitq);
 
-	dev = &gm12u320->dev;
 	ret = drm_dev_init(dev, &gm12u320_drm_driver, &interface->dev);
 	if (ret) {
+		put_device(gm12u320->dmadev);
 		kfree(gm12u320);
 		return ret;
 	}
@@ -713,15 +736,15 @@ static int gm12u320_usb_probe(struct usb_interface *interface,
 
 	ret = gm12u320_usb_alloc(gm12u320);
 	if (ret)
-		goto err_put;
+		goto err_put_device;
 
 	ret = gm12u320_set_ecomode(gm12u320);
 	if (ret)
-		goto err_put;
+		goto err_put_device;
 
 	ret = gm12u320_conn_init(gm12u320);
 	if (ret)
-		goto err_put;
+		goto err_put_device;
 
 	ret = drm_simple_display_pipe_init(&gm12u320->dev,
 					   &gm12u320->pipe,
@@ -731,19 +754,21 @@ static int gm12u320_usb_probe(struct usb_interface *interface,
 					   gm12u320_pipe_modifiers,
 					   &gm12u320->conn);
 	if (ret)
-		goto err_put;
+		goto err_put_device;
 
 	drm_mode_config_reset(dev);
 
 	usb_set_intfdata(interface, dev);
 	ret = drm_dev_register(dev, 0);
 	if (ret)
-		goto err_put;
+		goto err_put_device;
 
 	drm_fbdev_generic_setup(dev, 0);
 
 	return 0;
 
+err_put_device:
+        put_device(gm12u320->dmadev);
 err_put:
 	drm_dev_put(dev);
 	return ret;
@@ -752,9 +777,12 @@ err_put:
 static void gm12u320_usb_disconnect(struct usb_interface *interface)
 {
 	struct drm_device *dev = usb_get_intfdata(interface);
-	struct gm12u320_device *gm12u320 = dev->dev_private;
+	struct gm12u320_device *gm12u320 = to_gm12u320(dev);
 
 	gm12u320_stop_fb_update(gm12u320);
+
+	put_device(gm12u320->dmadev);
+	gm12u320->dmadev = NULL;
 	drm_dev_unplug(dev);
 	drm_dev_put(dev);
 }
