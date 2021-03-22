59605c8c5e91e50bc10b9949b0b3439945997af6
Shirish S <shirish.s@amd.com>
2021-03-22 18:44:03 +0530

    BACKPORT: FROMLIST: drm/amdgpu/powerplay/smu10: refactor AMDGPU_PP_SENSOR_GPU_LOAD
    refactor AMDGPU_PP_SENSOR_GPU_LOAD to ensure code consistency with other
    commands
    
    Signed-off-by: Shirish S <shirish.s@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    (am from https://patchwork.freedesktop.org/patch/424858/)
    
    Conflicts:
         drivers/gpu/drm/amd/pm/powerplay/hwmgr/smu10_hwmgr.c
         (shirish.s: Manually ported relevant changes)
    
        Backport note:
                I.) inorder to maintain consistency with existing code have made
                below changes:
                        1. no error handling of smum_send_msg_to_smc()
                        2. use -EINVAL instead of -ENOTSUPP
    
    BUG=b:180788703
    TEST='cat /sys/class/drm/card0/device/gpu_busy_percent' gives values
          varying per workload.
    
    Change-Id: I7e29de9dca10b6cadf7c5b07ca1ea65ced483bc3
    Signed-off-by: Shirish S <shirish.s@amd.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2777886
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>

--- drivers/gpu/drm/amd/powerplay/hwmgr/smu10_hwmgr.c
+++ drivers/gpu/drm/amd/powerplay/hwmgr/smu10_hwmgr.c
@@ -1153,15 +1153,14 @@ static int smu10_read_sensor(struct pp_hwmgr *hwmgr, int idx,
 		*size = 4;
 		break;
 	case AMDGPU_PP_SENSOR_GPU_LOAD:
-		if (has_gfx_busy) {
+		if (!has_gfx_busy)
+			ret = -EINVAL;
+		else {
 			smum_send_msg_to_smc(hwmgr, PPSMC_MSG_GetGfxBusy);
 			activity_percent = smum_get_argument(hwmgr);
-			activity_percent = activity_percent > 100 ? 100 : activity_percent;
-			*((uint32_t *)value) = activity_percent;
-			return 0;
-		} else {
-			return -EINVAL;
+			*((uint32_t *)value) = min(activity_percent, (u32)100);
 		}
+		break;
 	default:
 		ret = -EINVAL;
 		break;
