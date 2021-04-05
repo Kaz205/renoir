50673cccb10ccb11f0fd27106aefe0ae3cc62d26
Yu Zhao <yuzhao@google.com>
2021-04-05 11:44:28 -0600

    BACKPORT: FROMLIST: mm: multigenerational lru: activation
    For pages mapped upon page faults, the accessed bit is set during the
    initial faults. We add them to the per-zone lists index by max_seq,
    i.e., the youngest generation, so that eviction will not consider them
    before the aging has scanned them. Readahead pages allocated in the
    page fault path will also be added to the youngest generation, since
    it is assumed that they may be needed soon.
    
    For pages accessed multiple times via file descriptors, instead of
    activating them upon the second access, we activate them based on the
    refault rates of their tiers. Each generation contains at most
    MAX_NR_TIERS tiers, and they require additional MAX_NR_TIERS-2 bits in
    page->flags. Pages accessed N times via file descriptors belong to
    tier order_base_2(N). Tier 0 is the base tier and it contains pages
    read ahead, accessed once via file descriptors and accessed only via
    page tables. Pages from the base tier are evicted regardless of the
    refault rate. Pages from upper tiers that have higher refault rates
    than the base tier will be moved to the next generation. A feedback
    loop modeled after the PID controller monitors refault rates across
    all tiers and decides when to activate pages from which upper tiers
    in the reclaim path. The advantages of this model are:
      1) It has a negligible cost in the buffered IO access path because
      activations are done optionally in the reclaim path.
      2) It takes mapped pages into account and avoids overprotecting
      pages accessed multiple times via file descriptors.
      3) More tiers offer better protection to pages accessed more than
      twice when workloads doing intensive buffered IO are under memory
      pressure.
    
    Finally, we need to make sure deactivation works when the
    multigenerational lru is enabled. We cannot use PageActive() because
    it is not set on pages from active generations, in order to spare the
    aging the trouble of clearing it when active generations become
    inactive. So we deactivate pages unconditionally since deactivation is
    not a hot code path worth additional optimizations.
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Tested-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>
    (am from https://lore.kernel.org/patchwork/patch/1432183/)
    
    BUG=b:123039911
    TEST=Built
    
    Change-Id: Ibc9c90757fd095cdcc0a49823ada6b55f17ffc06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2987187
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>

--- mm/memory.c
+++ mm/memory.c
@@ -71,6 +71,7 @@
 #include <linux/dax.h>
 #include <linux/oom.h>
 #include <linux/numa.h>
+#include <linux/mm_inline.h>
 
 #include <asm/io.h>
 #include <asm/mmu_context.h>
@@ -2996,6 +2997,19 @@ void unmap_mapping_range(struct address_space *mapping,
 }
 EXPORT_SYMBOL(unmap_mapping_range);
 
+static void lru_gen_swap_refault(struct page *page, swp_entry_t entry)
+{
+	if (lru_gen_enabled()) {
+		void *item;
+		struct address_space *mapping = swap_address_space(entry);
+		pgoff_t index = swp_offset(entry);
+
+		item = xa_load(&mapping->i_pages, index);
+		if (xa_is_value(item))
+			lru_gen_refault(page, item);
+	}
+}
+
 /*
  * We enter with non-exclusive mmap_sem (to exclude vma changes,
  * but allow concurrent faults), and pte mapped but not yet locked.
@@ -3061,6 +3075,7 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 				__SetPageLocked(page);
 				__SetPageSwapBacked(page);
 				set_page_private(page, entry.val);
+				lru_gen_swap_refault(page, entry);
 				lru_cache_add_anon(page);
 				swap_readpage(page, true);
 			}
@@ -4499,9 +4514,9 @@ vm_fault_t __handle_speculative_fault(struct mm_struct *mm,
 		goto out_put;
 	}
 
-	mem_cgroup_enter_user_fault();
+	task_enter_user_fault();
 	ret = handle_pte_fault(&vmf);
-	mem_cgroup_exit_user_fault();
+	task_exit_user_fault();
 
 	put_vma(vma);
 
@@ -4556,7 +4571,7 @@ vm_fault_t handle_mm_fault(struct vm_area_struct *vma, unsigned long address,
 	 * space.  Kernel faults are handled more gracefully.
 	 */
 	if (flags & FAULT_FLAG_USER)
-		mem_cgroup_enter_user_fault();
+		task_enter_user_fault();
 
 	if (unlikely(is_vm_hugetlb_page(vma)))
 		ret = hugetlb_fault(vma->vm_mm, vma, address, flags);
@@ -4564,7 +4579,7 @@ vm_fault_t handle_mm_fault(struct vm_area_struct *vma, unsigned long address,
 		ret = __handle_mm_fault(vma, address, flags);
 
 	if (flags & FAULT_FLAG_USER) {
-		mem_cgroup_exit_user_fault();
+		task_exit_user_fault();
 		/*
 		 * The task may have entered a memcg OOM situation but
 		 * if the allocation error was handled gracefully (no
