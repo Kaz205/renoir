c3c1d988f649feb3e804b129df19e387e2f1248e
Ben Chuang <benchuanggli@gmail.com>
2021-05-11 14:18:35 +0800

    FROMGIT: mmc: sdhci-pci-gli: Fine tune GL9763E L1 entry delay
    Fine tune the value to 21us in order to improve read/write performance.
    
    Signed-off-by: Ben Chuang <benchuanggli@gmail.com>
    Link: https://lore.kernel.org/r/20210511061835.5559-1-benchuanggli@gmail.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit 713830797b5d6e61efe32ff771188451183c27bf
    git://git.kernel.org/pub/scm/linux/kernel/git/ulfh/mmc.git next)
    
    BUG=b:185309059
    TEST=build, run PLT and test read/write performance
    
    Change-Id: Ie8c67b0158ce0529ad13dc96434ee742a915671d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2886975
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>
    Reviewed-by: Alex Levin <levinale@google.com>
    Tested-by: Ben Chuang <benchuanggli@gmail.com>
    Commit-Queue: Ben Chuang <benchuanggli@gmail.com>

--- drivers/mmc/host/sdhci-pci-gli.c
+++ drivers/mmc/host/sdhci-pci-gli.c
@@ -90,7 +90,7 @@
 
 #define PCIE_GLI_9763E_CFG2      0x8A4
 #define   GLI_9763E_CFG2_L1DLY     GENMASK(28, 19)
-#define   GLI_9763E_CFG2_L1DLY_MID 0x50
+#define   GLI_9763E_CFG2_L1DLY_MID 0x54
 
 #define PCI_GLI_9755_WT       0x800
 #define   PCI_GLI_9755_WT_EN    BIT(0)
@@ -694,7 +694,7 @@ static void gli_set_gl9763e(struct sdhci_pci_slot *slot)
 
 	pci_read_config_dword(pdev, PCIE_GLI_9763E_CFG2, &value);
 	value &= ~GLI_9763E_CFG2_L1DLY;
-	/* set ASPM L1 entry delay to 20us */
+	/* set ASPM L1 entry delay to 21us */
 	value |= FIELD_PREP(GLI_9763E_CFG2_L1DLY, GLI_9763E_CFG2_L1DLY_MID);
 	pci_write_config_dword(pdev, PCIE_GLI_9763E_CFG2, value);
 
