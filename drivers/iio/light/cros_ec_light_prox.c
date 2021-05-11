Kaz205/renoir

commit bd3364777e0ee7eb7aa7d01196fbb64d9e7094db

CHROMIUM: iio: cros_ec_light: Add FIFO support for RGB sensor
Add callback to push samples to dedicated dev/iio:device:
Instead of the generic callback, for RGB sensors, we need to populate a
given samples with 2 events from the EC.

Partial revert of aa18c82702 ("BACKPORT: FROMLIST: iio: cros_ec:
 Register to cros_ec_sensorhub when EC supports FIFO"), readding the
push vector entry point.

BUG=chromium:946178,b:129419982
TEST=tast run <ip> hardware.SensorRing with sensor ring enabled.
     iio-info with sensor ring disabled.

Change-Id: I4fbd392d72ba4534a0d8a9f391bdad6e8e9d8ad0
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2223411

drivers/iio/light/cros_ec_light_prox.c
@@ -17,6 +17,7 @@
 #include <linux/module.h>
 #include <linux/platform_data/cros_ec_commands.h>
 #include <linux/platform_data/cros_ec_proto.h>
+#include <linux/platform_data/cros_ec_sensorhub.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 
@@ -306,6 +307,64 @@ static int cros_ec_light_prox_write(struct iio_dev *indio_dev,
 	return ret;
 }
 
+static int cros_ec_light_push_data(
+		struct iio_dev *indio_dev,
+		s16 *data,
+		s64 timestamp)
+{
+	struct cros_ec_sensors_core_state *st = iio_priv(indio_dev);
+	unsigned long scan_mask;
+
+	if (!st || !indio_dev->active_scan_mask)
+		return 0;
+
+	scan_mask = *(indio_dev->active_scan_mask);
+	if (scan_mask & ((1 << indio_dev->num_channels) - 2)) {
+		/*
+		 * Only one channel at most is used.
+		 * Use regular push function.
+		 */
+		return cros_ec_sensors_push_data(indio_dev, data, timestamp);
+	}
+
+	if (test_bit(0, indio_dev->active_scan_mask)) {
+		/*
+		 * Save clear channel, will be used when RGB data arrives.
+		 */
+		st->samples[0] = data[0];
+	}
+	return 0;
+}
+
+static int cros_ec_light_push_data_rgb(
+		struct iio_dev *indio_dev,
+		s16 *data,
+		s64 timestamp)
+{
+	struct cros_ec_sensors_core_state *st = iio_priv(indio_dev);
+	s16 *out;
+	unsigned long scan_mask;
+	unsigned int i;
+
+	if (!st || !indio_dev->active_scan_mask)
+		return 0;
+
+	scan_mask = *(indio_dev->active_scan_mask);
+	/*
+	 * Send all data needed.
+	 */
+	out = (s16 *)st->samples;
+	for_each_set_bit(i,
+			 indio_dev->active_scan_mask,
+			 indio_dev->masklength) {
+		if (i > 0)
+			*out = data[i - 1];
+		out++;
+	}
+	iio_push_to_buffers_with_timestamp(indio_dev, st->samples, timestamp);
+	return 0;
+}
+
 static irqreturn_t cros_ec_light_capture(int irq, void *p)
 {
 	struct iio_poll_func *pf = p;
@@ -364,6 +423,7 @@ static const struct iio_info cros_ec_light_prox_info = {
 static int cros_ec_light_prox_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
+	struct cros_ec_sensorhub *sensor_hub = dev_get_drvdata(dev->parent);
 	struct iio_dev *indio_dev;
 	struct cros_ec_light_prox_state *state;
 	struct iio_chan_spec *channel;
@@ -374,8 +434,8 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	ret = cros_ec_sensors_core_init(pdev, indio_dev, true,
-					cros_ec_light_capture
-					cros_ec_sensors_push_data);
+					cros_ec_light_capture,
+					cros_ec_light_push_data);
 	if (ret)
 		return ret;
 
@@ -430,6 +490,8 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 	channel++;
 
 	if (num_channels > CROS_EC_LIGHT_PROX_MIN_CHANNELS) {
+		u8 sensor_num = state->core.param.info.sensor_num;
+
 		for (i = CROS_EC_SENSOR_X; i < CROS_EC_SENSOR_MAX_AXIS;
 				i++, channel++) {
 			cros_ec_light_channel_common(channel);
@@ -438,6 +500,10 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 			channel->channel2 = IIO_MOD_LIGHT_RED + i;
 			channel->type = IIO_LIGHT;
 		}
+		cros_ec_sensorhub_register_push_data(
+				sensor_hub, sensor_num + 1,
+				indio_dev,
+				cros_ec_light_push_data_rgb);
 	}
 
 	/* Timestamp */

commit eab6efdb231b6dc9368a12fdb968bbc7e55d8bad

CHROMIUM: iio: cros_ec_light: Fix regular and RGB callbacks
Calculation based on scan mask was incorrect.
Instead use the usual callback for single channel ALS, and
just save the first channel for RGB sensor.

Fix second RGB callback by preserving the first channel when necessary.

Fixes: bd3364777 ("CHROMIUM: iio: cros_ec_light: Add FIFO support for RGB sensor")

BUG=b:172790261
TEST=On nocturne with 4.19 (ALS sensor OPT3001), with USE flag
iioservice set, check we can retrieve samples:
   echo 1 > scan_elements/in_illuminance_en
   echo 1 > scan_elements/in_timestamp_en
   echo 1 > sampling_frequency
   echo 1 > buffer/enable
   od -x /dev/iio\:device4

Change-Id: Ie0f5247d21df0adb2c518d7dae3946c58c9ce950
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2568226
Reviewed-by: Cheng-Hao Yang <chenghaoyang@chromium.org>
(cherry picked from commit 098a98758e31584130da0cd3f3f9c2788c9b9fe0)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2721240

drivers/iio/light/cros_ec_light_prox.c
@@ -300,26 +300,15 @@ static int cros_ec_light_push_data(
 		s64 timestamp)
 {
 	struct cros_ec_sensors_core_state *st = iio_priv(indio_dev);
-	unsigned long scan_mask;
 
 	if (!st || !indio_dev->active_scan_mask)
 		return 0;
 
-	scan_mask = *(indio_dev->active_scan_mask);
-	if (scan_mask & ((1 << indio_dev->num_channels) - 2)) {
-		/*
-		 * Only one channel at most is used.
-		 * Use regular push function.
-		 */
-		return cros_ec_sensors_push_data(indio_dev, data, timestamp);
-	}
-
-	if (test_bit(0, indio_dev->active_scan_mask)) {
-		/*
-		 * Save clear channel, will be used when RGB data arrives.
-		 */
+	/* Save clear channel, will be used when RGB data arrives. */
+	if (test_bit(0, indio_dev->active_scan_mask))
 		st->samples[0] = data[0];
-	}
+
+	/* Wait for RGB callback to send samples upstream. */
 	return 0;
 }
 
@@ -330,25 +319,33 @@ static int cros_ec_light_push_data_rgb(
 {
 	struct cros_ec_sensors_core_state *st = iio_priv(indio_dev);
 	s16 *out;
-	unsigned long scan_mask;
-	unsigned int i;
+	s64 delta;
+	unsigned int i = 1;
 
 	if (!st || !indio_dev->active_scan_mask)
 		return 0;
 
-	scan_mask = *(indio_dev->active_scan_mask);
 	/*
 	 * Send all data needed.
 	 */
 	out = (s16 *)st->samples;
-	for_each_set_bit(i,
+	if (test_bit(0, indio_dev->active_scan_mask))
+		out++;
+
+	for_each_set_bit_from(i,
 			 indio_dev->active_scan_mask,
 			 indio_dev->masklength) {
-		if (i > 0)
-			*out = data[i - 1];
+		*out = data[i - 1];
 		out++;
 	}
-	iio_push_to_buffers_with_timestamp(indio_dev, st->samples, timestamp);
+
+	if (iio_device_get_clock(indio_dev) != CLOCK_BOOTTIME)
+		delta = iio_get_time_ns(indio_dev) - cros_ec_get_time_ns();
+	else
+		delta = 0;
+
+	iio_push_to_buffers_with_timestamp(indio_dev, st->samples,
+					   timestamp + delta);
 	return 0;
 }
 
@@ -422,7 +419,7 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 
 	ret = cros_ec_sensors_core_init(pdev, indio_dev, true, false,
 					cros_ec_light_capture,
-					cros_ec_light_push_data);
+					cros_ec_sensors_push_data);
 	if (ret)
 		return ret;
 
@@ -487,6 +484,10 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 			channel->channel2 = IIO_MOD_LIGHT_RED + i;
 			channel->type = IIO_LIGHT;
 		}
+		cros_ec_sensorhub_register_push_data(
+				sensor_hub, sensor_num, false,
+				indio_dev,
+				cros_ec_light_push_data);
 		cros_ec_sensorhub_register_push_data(
 				sensor_hub, sensor_num + 1, false,
 				indio_dev,