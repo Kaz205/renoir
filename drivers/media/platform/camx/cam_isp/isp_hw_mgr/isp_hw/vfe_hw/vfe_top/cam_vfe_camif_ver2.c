04d478497ab40e6725cdde98d31e35a71dd092d1
Ricardo Ribalda <ribalda@chromium.org>
2021-05-28 07:56:22 +0000

    CHROMIUM: camx: Fix -Wformat
    Fix all -Wformat-extra-args, -Wformat-insufficient-args and -Wformat
    warnings.
    
    BUG=b:182475392
    TEST=Use the camera app and verify that there is still a valid image
    TEST=FEATURES="noclean" USE="nowerror kernel_warning_level_1" cros_workon_make --board=strongbad chromeos-kernel-5_4; find drivers/media/platform/camx -exec touch {} +; FEATURES="noclean" USE="nowerror kernel_warning_level_1" cros_workon_make --board=strongbad chromeos-kernel-5_4
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>
    Change-Id: I8e725512db3d36eaab3550b713174243deb1dbbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2923528
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Commit-Queue: Douglas Anderson <dianders@chromium.org>

--- drivers/media/platform/camx/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/vfe_top/cam_vfe_camif_ver2.c
+++ drivers/media/platform/camx/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/vfe_top/cam_vfe_camif_ver2.c
@@ -342,14 +342,15 @@ static int cam_vfe_camif_reg_dump(
 	for (wm_idx = 0; wm_idx <= 23; wm_idx++) {
 		offset = 0x2214 + 0x100 * wm_idx;
 		CAM_INFO(CAM_ISP,
-			"BUS_WM%u offset 0x%x val 0x%x offset 0x%x val 0x%x",
-			wm_idx, offset,
-			cam_io_r_mb(camif_priv->mem_base + offset),
-			offset + 4, cam_io_r_mb(camif_priv->mem_base +
-			offset + 4), offset + 8,
-			cam_io_r_mb(camif_priv->mem_base + offset + 8),
-			offset + 12, cam_io_r_mb(camif_priv->mem_base +
-			offset + 12));
+			 "BUS_WM%u offset 0x%x val 0x%x offset 0x%x val 0x%x offset 0x%x val 0x%x offset 0x%x val 0x%x",
+			 wm_idx, offset,
+			 cam_io_r_mb(camif_priv->mem_base + offset),
+			 offset + 4,
+			 cam_io_r_mb(camif_priv->mem_base + offset + 4),
+			 offset + 8,
+			 cam_io_r_mb(camif_priv->mem_base + offset + 8),
+			 offset + 12,
+			 cam_io_r_mb(camif_priv->mem_base + offset + 12));
 	}
 
 	offset = 0x420;
--- drivers/media/platform/camx/cam_sync/cam_sync.c
+++ drivers/media/platform/camx/cam_sync/cam_sync.c
@@ -40,7 +40,7 @@ static void cam_sync_print_fence_table(void)
 			sync_dev->sync_table[cnt].name,
 			sync_dev->sync_table[cnt].type,
 			sync_dev->sync_table[cnt].state,
-			sync_dev->sync_table[cnt].ref_cnt);
+			atomic_read(&sync_dev->sync_table[cnt].ref_cnt));
 	}
 }
 
@@ -54,7 +54,7 @@ int cam_sync_create(int32_t *sync_obj, const char *name)
 		idx = find_first_zero_bit(sync_dev->bitmap, CAM_SYNC_MAX_OBJS);
 		if (idx >= CAM_SYNC_MAX_OBJS) {
 			CAM_ERR(CAM_SYNC,
-				"Error: Unable to Create Sync Idx = %d Reached Max!!",
+				"Error: Unable to Create Sync Idx = %ld Reached Max!!",
 				idx);
 			sync_dev->err_cnt++;
 			if (sync_dev->err_cnt == 1)
