c692528ae791059dca96a892fb0bc34a67f91616
Yu Zhao <yuzhao@google.com>
2021-05-28 12:35:18 -0600

    CHROMIUM: mm: multigenerational lru: don't use min_filelist_kbytes
    The multigenerational lru doesn't need min_filelist_kbytes.
    
    BUG=b:123039911
    TEST=Built
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Change-Id: I221ddc3d041a1d4b506427b9d0aa7306b27814c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2987194
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3142,6 +3142,8 @@ static inline int pages_identical(struct page *page1, struct page *page2)
 }
 
 extern int min_filelist_kbytes;
+extern int min_filelist_kbytes_handler(struct ctl_table *table, int write,
+		void __user *buf, size_t *len, loff_t *pos);
 
 /**
  * seal_check_future_write - Check for F_SEAL_FUTURE_WRITE flag and handle it
