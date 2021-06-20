4a7e1602f50a4b9e903d87247757ffb8c87c7c1d
Juhyung Park <qkrwngud825@gmail.com>
2021-06-21 00:48:27 +0900

    power: reset: qcom-dload-mode: perform a warm, nodump reboot upon a panic
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    [Kazuki: Fix unused variable warning]
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/power/reset/qcom-dload-mode.c
+++ drivers/power/reset/qcom-dload-mode.c
@@ -248,6 +248,12 @@ static int qcom_dload_panic(struct notifier_block *this, unsigned long event,
 	poweroff->in_panic = true;
 	if (enable_dump)
 		msm_enable_dump_mode(true);
+
+	// Perform a warm reboot
+	set_download_mode(QCOM_DOWNLOAD_NODUMP);
+	reboot_mode = REBOOT_WARM;
+	mb();
+
 	return NOTIFY_OK;
 }
 
@@ -255,12 +261,9 @@ static int qcom_dload_reboot(struct notifier_block *this, unsigned long event,
 			      void *ptr)
 {
 	char *cmd = ptr;
-	struct qcom_dload *poweroff = container_of(this, struct qcom_dload,
-						     reboot_nb);
 
 	/* Clean shutdown, disable dump mode to allow normal restart */
-	if (!poweroff->in_panic)
-		set_download_mode(QCOM_DOWNLOAD_NODUMP);
+	set_download_mode(QCOM_DOWNLOAD_NODUMP);
 
 	if (cmd) {
 		if (!strcmp(cmd, "qcom_dload"))
