ca7b7f0494ae2575e2be07619bf32ff5e006acbb
Ricardo Ribalda <ribalda@chromium.org>
2021-06-21 20:41:07 +0000

    CHROMIUM: camx: Unexport static variables
    These variables are only used in this file and do not need to be
    exported.
    
    BUG=b:182476057
    TEST=ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make W=1 C=1
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>
    Change-Id: I5c95a87b64d17933ac0793185fe963303c1d978c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2976973
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- drivers/media/platform/camx/cam_icp/icp_hw/a5_hw/a5_dev.c
+++ drivers/media/platform/camx/cam_icp/icp_hw/a5_hw/a5_dev.c
@@ -26,10 +26,9 @@
 #include "cam_cpas_api.h"
 #include "cam_debug_util.h"
 
-struct a5_soc_info cam_a5_soc_info;
-EXPORT_SYMBOL(cam_a5_soc_info);
+static struct a5_soc_info cam_a5_soc_info;
 
-struct cam_a5_device_hw_info cam_a5_hw_info = {
+static const struct cam_a5_device_hw_info cam_a5_hw_info = {
 	.hw_ver = 0x0,
 	.nsec_reset = 0x4,
 	.a5_control = 0x8,
@@ -49,7 +48,6 @@ struct cam_a5_device_hw_info cam_a5_hw_info = {
 	.uncached_heap_size = 0x60,
 	.a5_status = 0x200,
 };
-EXPORT_SYMBOL(cam_a5_hw_info);
 
 static bool cam_a5_cpas_cb(uint32_t client_handle, void *userdata,
 	struct cam_cpas_irq_data *irq_data)
