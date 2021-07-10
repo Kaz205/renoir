1ef023a90bad0b7ddd5e5ae10efb1b1e12eeba40
Kazuki Hashimoto <kaz205@tuta.io>
2021-07-10 19:16:23 +0900

    renoir
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- drone.sh
+++ drone.sh
@@ -8,17 +8,14 @@ git submodule init && git submodule update
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 make clean -j$(nproc --all) && make mrproper -j$(nproc --all)
-make floral_defconfig -j$(nproc --all)
-make -j$(nproc --all) Image.lz4 dtbs dtbo.img
+make renoir_defconfig -j$(nproc --all)
+make -j$(nproc --all) Image.gz
 
 if [ $? -ne 0 ]
 then
     exit 1
 fi
 
-cp out/arch/arm64/boot/Image.lz4 $ak/Image.lz4
-find out/arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > $ak/dtb
-cp out/arch/arm64/boot/dtb $ak/dtb
-cp out/arch/arm64/boot/dtbo.img $ak/dtbo.img
+cp out/arch/arm64/boot/Image.gz $ak/Image.gz
 cd $ak
 zip -FSr9 kernel.zip ./*
