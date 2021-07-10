4b5085a52b591a27c45d4b51688924a7fd56990a
Kazuki Hashimoto <kaz205@tuta.io>
2021-07-10 22:11:26 +0900

    sched/walt: Fix compilation
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- kernel/sched/walt/trace.h
+++ kernel/sched/walt/trace.h
@@ -663,6 +663,7 @@ TRACE_EVENT(walt_window_rollover,
 #endif /* _TRACE_WALT_H */
 
 #undef TRACE_INCLUDE_PATH
+#undef TRACE_INCLUDE_FILE
 #define TRACE_INCLUDE_PATH .
 #define TRACE_INCLUDE_FILE trace
 
