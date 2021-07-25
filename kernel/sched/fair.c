fc08e71772f00e75bccc97bd24822e1a438b0c6b
Kazuki Hashimoto <kaz205@tuta.io>
2021-07-25 18:09:39 +0900

    sched go brr
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8210,12 +8210,14 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 			 !preferred_cluster(
 				cpu_rq(env->dst_cpu)->wrq.cluster, p))
 		return 0;
+#endif
 
 	/* Don't detach task if it doesn't fit on the destination */
 	if (env->flags & LBF_IGNORE_BIG_TASKS &&
 		!task_fits_max(p, env->dst_cpu))
 		return 0;
 
+#ifdef CONFIG_SCHED_WALT
 	/* Don't detach task if it is under active migration */
 	if (env->src_rq->wrq.push_task == p)
 		return 0;
