387109f8be9fdb81e0d5b8ff681fd3001973f525
Juston Li <juston.li@intel.com>
2021-08-31 09:48:23 -0700

    Revert "FROMLIST: mei: pxp: add vtag parameter to mei_pxp_send/receive interface"
    This reverts commit 2aa72c6b9a10461fbed3ce653df57f7ae136e92f.
    
    BUG=b:191508777
    TEST=tast run <IP> video.PlayDRM*
    
    Signed-off-by: Juston Li <juston.li@intel.com>
    Change-Id: I70d8c7af52a33314f02b1c31fd41a899eeab51d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3207057
    Tested-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>

--- drivers/misc/mei/pxp/mei_pxp.c
+++ drivers/misc/mei/pxp/mei_pxp.c
@@ -27,11 +27,10 @@
  * @dev: device corresponding to the mei_cl_device
  * @message: a message buffer to send
  * @size: size of the message
- * @vtag: the vtag of the connection (use 0 for default)
  * Return: 0 on Success, <0 on Failure
  */
 static int
-mei_pxp_send_message(struct device *dev, const void *message, size_t size, u8 vtag)
+mei_pxp_send_message(struct device *dev, const void *message, size_t size)
 {
 	struct mei_cl_device *cldev;
 	ssize_t byte;
@@ -42,7 +41,7 @@ mei_pxp_send_message(struct device *dev, const void *message, size_t size, u8 vt
 	cldev = to_mei_cl_device(dev);
 
 	/* temporary drop const qualifier till the API is fixed */
-	byte = mei_cldev_send_vtag(cldev, (u8 *)message, size, vtag);
+	byte = mei_cldev_send(cldev, (u8 *)message, size);
 	if (byte < 0) {
 		dev_dbg(dev, "mei_cldev_send failed. %zd\n", byte);
 		return byte;
@@ -56,11 +55,10 @@ mei_pxp_send_message(struct device *dev, const void *message, size_t size, u8 vt
  * @dev: device corresponding to the mei_cl_device
  * @buffer: a message buffer to contain the received message
  * @size: size of the buffer
- * @vtag: the vtag of the connection (use 0 for default)
  * Return: bytes sent on Success, <0 on Failure
  */
 static int
-mei_pxp_receive_message(struct device *dev, void *buffer, size_t size, u8 vtag)
+mei_pxp_receive_message(struct device *dev, void *buffer, size_t size)
 {
 	struct mei_cl_device *cldev;
 	ssize_t byte;
@@ -70,7 +68,7 @@ mei_pxp_receive_message(struct device *dev, void *buffer, size_t size, u8 vtag)
 
 	cldev = to_mei_cl_device(dev);
 
-	byte = mei_cldev_recv_vtag(cldev, buffer, size, &vtag);
+	byte = mei_cldev_recv(cldev, buffer, size);
 	if (byte < 0) {
 		dev_dbg(dev, "mei_cldev_recv failed. %zd\n", byte);
 		return byte;
