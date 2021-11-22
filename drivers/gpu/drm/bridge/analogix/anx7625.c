f978573729cd06ea5712ff6474c16e325ec07c0d
Hsin-Yi Wang <hsinyi@chromium.org>
2021-11-22 14:52:20 +0800

    FIXUP: FROMLIST: drm/bridge: anx7625: Fix edid_read break case in sp_tx_edid_read()
    Fix the variable type.
    
    The fix now matches v2[1] of the original patch.
    
    [1] https://patchwork.kernel.org/project/dri-devel/patch/20211118193002.407168-1-hsinyi@chromium.org/
    
    BUG=b:206970142
    TEST=emerge kernel and test on jacuzzi.
    
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Change-Id: I12b7dbb20f8692883a0ae09b377d782b9e5f895a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3292909
    Commit-Queue: Guenter Roeck <groeck@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    (cherry picked from commit 3005b25129a692a5f0a2947d386919333f41e332)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3295824

--- drivers/gpu/drm/bridge/analogix/anx7625.c
+++ drivers/gpu/drm/bridge/analogix/anx7625.c
@@ -1021,7 +1021,7 @@ static int sp_tx_edid_read(struct anx7625_data *ctx,
 	int count, blocks_num;
 	u8 pblock_buf[MAX_DPCD_BUFFER_SIZE];
 	u8 i, j;
-	u8 g_edid_break = 0;
+	int g_edid_break = 0;
 	int ret;
 	struct device *dev = &ctx->client->dev;
 
