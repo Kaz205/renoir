hentaiOS-Devices/kernel_google_gs

BACKPORT: ASoC: intel: sof_rt5682: update platform device name for Maxim amplifier
To follow 20-character length limitation of platform device name, we
have only 7 character space for amplifier. Therefore, the last
character of mx98357a and mx98360a is removed to save space.

Signed-off-by: Brent Lu <brent.lu@intel.com>
Fixes: e224ef76fa8a ('ASoC: intel: sof_rt5682: support jsl_rt5682s_mx98360a board')
Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
Link: https://lore.kernel.org/r/20210927143249.439129-1-brent.lu@intel.com
Signed-off-by: Mark Brown <broonie@kernel.org>

(cherry picked from commit 3c561a090c7920624b83005a279a66cc8a7bed2b)

Conflicts:
   sound/soc/intel/boards/sof_rt5682.c
   sound/soc/intel/common/soc-acpi-intel-jsl-match.c

BUG=b:200778066
TEST=ODM tests audio function on Anahera

Change-Id: I1ca53d05b7c6e936cc0bbe15009cccdafb1729d7
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3252575
Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
Tested-by: Brent Lu <brent.lu@intel.com>
Commit-Queue: Curtis Malainey <cujomalainey@chromium.org>

sound/soc/intel/boards/sof_rt5682.c
@@ -1019,7 +1019,7 @@ static const struct platform_device_id board_ids[] = {
 		.name = "sof_rt5682",
 	},
 	{
-		.name = "tgl_mx98357a_rt5682",
+		.name = "tgl_mx98357_rt5682",
 		.driver_data = (kernel_ulong_t)(SOF_RT5682_MCLK_EN |
 					SOF_RT5682_SSP_CODEC(0) |
 					SOF_SPEAKER_AMP_PRESENT |
@@ -1049,7 +1049,7 @@ static const struct platform_device_id board_ids[] = {
 					SOF_SSP_BT_OFFLOAD_PRESENT),
 	},
 	{
-		.name = "jsl_rt5682_mx98360a",
+		.name = "jsl_rt5682_mx98360",
 		.driver_data = (kernel_ulong_t)(SOF_RT5682_MCLK_EN |
 					SOF_RT5682_MCLK_24MHZ |
 					SOF_RT5682_SSP_CODEC(0) |
@@ -1099,7 +1099,7 @@ static const struct platform_device_id board_ids[] = {
 					SOF_SSP_BT_OFFLOAD_PRESENT),
 	},
 	{
-		.name = "adl_mx98357a_rt5682",
+		.name = "adl_mx98357_rt5682",
 		.driver_data = (kernel_ulong_t)(SOF_RT5682_MCLK_EN |
 					SOF_RT5682_SSP_CODEC(0) |
 					SOF_SPEAKER_AMP_PRESENT |

Kaz205/renoir

(cherry picked from commit 3c561a090c7920624b83005a279a66cc8a7bed2b
https://git.kernel.org/pub/scm/linux/kernel/git/broonie/sound.git for-next)
 Conflicts:
        sound/soc/intel/boards/sof_rt5682.c
        sound/soc/intel/common/soc-acpi-intel-jsl-match.c
        sound/soc/intel/common/soc-acpi-intel-tgl-match.c
BUG=b:192535692
TEST=verify basic audio function
Signed-off-by: Mac Chiang <mac.chiang@intel.com>
Change-Id: I45346e411381ebc5beed1a0c3fd3377cccb8259c
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3319919
Tested-by: Daniel Peng <daniel_peng@pegatron.corp-partner.google.com>

sound/soc/intel/boards/sof_rt5682.c
@@ -859,7 +859,7 @@ static const struct platform_device_id board_ids[] = {
 		.name = "sof_rt5682",
 	},
 	{
-		.name = "tgl_mx98357a_rt5682",
+		.name = "tgl_mx98357_rt5682",
 		.driver_data = (kernel_ulong_t)(SOF_RT5682_MCLK_EN |
 					SOF_RT5682_SSP_CODEC(0) |
 					SOF_SPEAKER_AMP_PRESENT |
@@ -885,7 +885,7 @@ static const struct platform_device_id board_ids[] = {
 					SOF_RT5682_NUM_HDMIDEV(4)),
 	},
 	{
-		.name = "jsl_rt5682_mx98360a",
+		.name = "jsl_rt5682_mx98360",
 		.driver_data = (kernel_ulong_t)(SOF_RT5682_MCLK_EN |
 					SOF_RT5682_MCLK_24MHZ |
 					SOF_RT5682_SSP_CODEC(0) |
@@ -932,7 +932,7 @@ static const struct platform_device_id board_ids[] = {
 					SOF_RT5682_SSP_AMP(1)),
 	},
 	{
-		.name = "jsl_rt5682s_mx98360a",
+		.name = "jsl_rt5682s_mx98360",
 		.driver_data = (kernel_ulong_t)(SOF_RT5682_MCLK_EN |
 					SOF_RT5682_MCLK_24MHZ |
 					SOF_RT5682_SSP_CODEC(0) |