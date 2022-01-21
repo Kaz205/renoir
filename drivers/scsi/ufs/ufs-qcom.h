e1d34d9bed5743120b6b06751b9fa3d51a0c87a5
Kazuki Hashimoto <kazukih@tuta.io>
2022-01-22 01:54:08 +0900

    scsi: ufs-qcom: Remove pm_qos clusterfuck
    These pieces of codes affine a pm_qos before a transfer request is
    called, then disables it after the UFS clock is turned off, leaving the
    CPU in a shallow idle state for longer than necessary. Furthermore, this
    affines the pm_qos to all cores, wasting immense amounts of power.
    Remove the code entirely in order to replace it with a different
    implementation.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/scsi/ufs/ufs-qcom.h
+++ drivers/scsi/ufs/ufs-qcom.h
@@ -8,7 +8,6 @@
 #include <linux/reset-controller.h>
 #include <linux/reset.h>
 #include <linux/phy/phy.h>
-#include <linux/pm_qos.h>
 #include "ufshcd.h"
 #ifdef CONFIG_SCSI_UFSHCD_QTI
 #include "unipro.h"
@@ -282,7 +281,6 @@ struct qcom_bus_scale_data {
 struct qos_cpu_group {
 	cpumask_t mask;
 	unsigned int *votes;
-	struct dev_pm_qos_request *qos_req;
 	bool voted;
 	struct work_struct vwork;
 	struct ufs_qcom_host *host;
@@ -293,13 +291,6 @@ struct ufs_qcom_qos_req {
 	struct qos_cpu_group *qcg;
 	unsigned int num_groups;
 	struct workqueue_struct *workq;
-};
-
-/* Check for QOS_POWER when added to DT */
-enum constraint {
-	QOS_PERF,
-	QOS_POWER,
-	QOS_MAX,
 };
 
 struct ufs_qcom_host {
