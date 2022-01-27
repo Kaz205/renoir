d5595e75103b8f707fa1a74e2dbf4b3b39ec14f7
Kazuki Hashimoto <kazukih@tuta.io>
2022-01-27 18:24:20 +0900

    treewide: Silence overly verbose logs
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- techpack/audio/asoc/msm-dai-q6-v2.c
+++ techpack/audio/asoc/msm-dai-q6-v2.c
@@ -2,7 +2,6 @@
 /* Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
-
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/device.h>
