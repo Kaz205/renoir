b3951225c5add863679e7c59b0e46f4c93a7d1a8
Kazuki Hashimoto <kazukih@tuta.io>
2022-02-23 09:22:53 +0900

    Revert "mm: direct previous __GFP_CMA allocations to offlinable memory"
    This reverts commit 8b04672471389b5fd3fa0b7e60241fdc523c582a.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- include/linux/highmem.h
+++ include/linux/highmem.h
@@ -205,9 +205,12 @@ static inline struct page *
 alloc_zeroed_user_highpage_movable(struct vm_area_struct *vma,
 					unsigned long vaddr)
 {
-	return __alloc_zeroed_user_highpage(
-			__GFP_MOVABLE|__GFP_CMA|__GFP_OFFLINABLE, vma,
-			vaddr);
+#ifndef CONFIG_CMA_DIRECT_UTILIZATION
+	return __alloc_zeroed_user_highpage(__GFP_MOVABLE, vma, vaddr);
+#else
+	return __alloc_zeroed_user_highpage(__GFP_MOVABLE|__GFP_CMA, vma,
+						vaddr);
+#endif
 }
 
 static inline void clear_highpage(struct page *page)
