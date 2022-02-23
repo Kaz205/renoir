902649fea00bc6fceb2abfbbfbb2674e258ba07f
Kazuki Hashimoto <kazukih@tuta.io>
2022-02-23 09:03:57 +0900

    Revert "mm: Update is_vmalloc_addr to account for vmalloc savings"
    This reverts commit 674893e4529a0af02fbe883133f6e8bd62ec98cc.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -675,10 +675,6 @@ unsigned long vmalloc_to_pfn(const void *addr);
  * On nommu, vmalloc/vfree wrap through kmalloc/kfree directly, so there
  * is no special casing required.
  */
-
-#ifdef CONFIG_ENABLE_VMALLOC_SAVING
-extern bool is_vmalloc_addr(const void *x);
-#else
 static inline bool is_vmalloc_addr(const void *x)
 {
 #ifdef CONFIG_MMU
@@ -689,7 +685,6 @@ static inline bool is_vmalloc_addr(const void *x)
 	return false;
 #endif
 }
-#endif //CONFIG_ENABLE_VMALLOC_SAVING
 
 #ifndef is_ioremap_addr
 #define is_ioremap_addr(x) is_vmalloc_addr(x)
