0ddc982cfdd26c02a3f9908a47b05ee0b07924b0
Kazuki Hashimoto <kazukih@tuta.io>
2022-02-23 09:06:58 +0900

    Revert "FROMGIT: mm, oom_adj: don't loop through tasks in __set_oom_adj when not necessary"
    This reverts commit 74cf1bc3a21207f4384622c474ff1ce55e21a057.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1046,6 +1046,7 @@ static ssize_t oom_adj_read(struct file *file, char __user *buf, size_t count,
 
 static int __set_oom_adj(struct file *file, int oom_adj, bool legacy)
 {
+	static DEFINE_MUTEX(oom_adj_mutex);
 	struct mm_struct *mm = NULL;
 	struct task_struct *task;
 	int err = 0;
@@ -1085,7 +1086,7 @@ static int __set_oom_adj(struct file *file, int oom_adj, bool legacy)
 		struct task_struct *p = find_lock_task_mm(task);
 
 		if (p) {
-			if (test_bit(MMF_MULTIPROCESS, &p->mm->flags)) {
+			if (atomic_read(&p->mm->mm_users) > 1) {
 				mm = p->mm;
 				mmgrab(mm);
 			}
