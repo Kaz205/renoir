b33d5be79a826299de9c2ed85753c37617df6b7b
Kazuki Hashimoto <kazukih@tuta.io>
2022-02-23 10:32:58 +0900

    Revert "mm, oom: run the showmem notifier in oom"
    This reverts commit e270acd2227a982554ff43fa89b9baef76abeb8b.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -42,7 +42,6 @@
 #include <linux/kthread.h>
 #include <linux/init.h>
 #include <linux/mmu_notifier.h>
-#include <linux/show_mem_notifier.h>
 
 #include <asm/tlb.h>
 #include "internal.h"
@@ -463,10 +462,7 @@ static void dump_header(struct oom_control *oc, struct task_struct *p)
 		show_mem(SHOW_MEM_FILTER_NODES, oc->nodemask);
 		if (is_dump_unreclaim_slabs())
 			dump_unreclaimable_slab();
-
-		show_mem_call_notifiers();
 	}
-
 	if (sysctl_oom_dump_tasks)
 		dump_tasks(oc);
 	if (p)
