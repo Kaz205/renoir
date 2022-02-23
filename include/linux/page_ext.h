4fec77e398793b083e1dba2ed815a4201cc46ce6
Kazuki Hashimoto <kazukih@tuta.io>
2022-02-23 09:06:38 +0900

    Revert "mm: fix the page_owner initializing issue for arm32"
    This reverts commit 7f35a1228f2d872474ac10ffd669bd604d3d7275.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- include/linux/page_ext.h
+++ include/linux/page_ext.h
@@ -47,12 +47,8 @@ static inline void page_ext_init_flatmem(void)
 {
 }
 extern void page_ext_init(void);
-static inline void page_ext_init_flatmem_late(void)
-{
-}
 #else
 extern void page_ext_init_flatmem(void);
-extern void page_ext_init_flatmem_late(void);
 static inline void page_ext_init(void)
 {
 }
@@ -80,10 +76,6 @@ static inline struct page_ext *lookup_page_ext(const struct page *page)
 }
 
 static inline void page_ext_init(void)
-{
-}
-
-static inline void page_ext_init_flatmem_late(void)
 {
 }
 
