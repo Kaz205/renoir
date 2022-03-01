5edb20e53c8c5946cbe0877a056e7bb26666d3c0
Kazuki Hashimoto <kazukih@tuta.io>
2022-03-02 02:20:11 +0900

    drone: Rework
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drone.sh
+++ drone.sh
@@ -1,12 +1,13 @@
 export ARCH=arm64
 export SUBARCH=arm64
-export ak=AnyKernel3
-export PATH="/drone/src/google-clang/clang-r445002/bin:$PATH"
+export home=/drone/src
+export ak=$home/AnyKernel3
+export PATH="$home/google-clang/bin:$PATH"
 
 MAKE_PARAMS="CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 \
-	CROSS_COMPILE=/drone/src/arm64-gcc/bin/aarch64-elf- \
-	CROSS_COMPILE_COMPAT=/drone/src/arm32-gcc/bin/arm-eabi- \
-    LD=ld.lld \
+	CROSS_COMPILE=$home/arm64-gcc/bin/aarch64-elf- \
+	CROSS_COMPILE_COMPAT=$home/arm32-gcc/bin/arm-eabi- \
+	LD=ld.lld \
 	AR=llvm-ar \
 	NM=llvm-nm \
 	OBJCOPY=llvm-objcopy \
@@ -15,7 +16,6 @@ MAKE_PARAMS="CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 \
 git submodule init && git submodule update
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-make clean -j$(nproc --all) && make mrproper -j$(nproc --all)
 make $MAKE_PARAMS renoir_defconfig -j$(nproc --all)
 make $MAKE_PARAMS Image dtbs dtbo.img -j$(nproc --all)
 
