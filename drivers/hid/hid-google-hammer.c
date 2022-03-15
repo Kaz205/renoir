f7de42b3078bd4254d18a59e7050928974d34bb8
Stephen Boyd <swboyd@chromium.org>
2022-03-14 21:08:44 -0700

    UPSTREAM: HID: google: Add support for vivaldi to hid-hammer
    Add support to the hammer driver to parse vivaldi keyboard layouts and
    expose them to userspace. This allows hammer devices to use vivaldi
    function row keys while also supporting the other features this driver
    supports, like the CBAS (chrome base attached switch) and a keyboard
    backlight.
    
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Tested-by: Stephen Boyd <swboyd@chromium.org> # coachz, wormdingler
    Link: https://lore.kernel.org/r/20220228075446.466016-5-dmitry.torokhov@gmail.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    (cherry picked from commit a9d672998a33e4bccdf62f6c2f8a47d51893b83f)
    
    BUG=b:214593871, b:229004203
    TEST=emerge-strongbad chromeos-kernel-5_4; Boot on wormdingler;
         evtest shows no more cros_ec device
    
    Change-Id: I09b81d60facc3e7a85e5072824d5793a98de1c6d
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3625915
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/hid/hid-google-hammer.c
+++ drivers/hid/hid-google-hammer.c
@@ -15,6 +15,7 @@
 
 #include <linux/acpi.h>
 #include <linux/hid.h>
+#include <linux/input/vivaldi-fmap.h>
 #include <linux/leds.h>
 #include <linux/module.h>
 #include <linux/of.h>
@@ -25,6 +26,7 @@
 #include <asm/unaligned.h>
 
 #include "hid-ids.h"
+#include "hid-vivaldi-common.h"
 
 /*
  * C(hrome)B(ase)A(ttached)S(witch) - switch exported by Chrome EC and reporting
@@ -516,7 +518,14 @@ out:
 static int hammer_probe(struct hid_device *hdev,
 			const struct hid_device_id *id)
 {
+	struct vivaldi_data *vdata;
 	int error;
+
+	vdata = devm_kzalloc(&hdev->dev, sizeof(*vdata), GFP_KERNEL);
+	if (!vdata)
+		return -ENOMEM;
+
+	hid_set_drvdata(hdev, vdata);
 
 	error = hid_parse(hdev);
 	if (error)
@@ -611,6 +620,8 @@ static struct hid_driver hammer_driver = {
 	.id_table = hammer_devices,
 	.probe = hammer_probe,
 	.remove = hammer_remove,
+	.feature_mapping = vivaldi_feature_mapping,
+	.input_configured = vivaldi_input_configured,
 	.input_mapping = hammer_input_mapping,
 	.event = hammer_event,
 };
