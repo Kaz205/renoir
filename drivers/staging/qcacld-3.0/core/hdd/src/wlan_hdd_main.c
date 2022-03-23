2196ec960fc2f5b37dfc23765ffd90da573a34a8
Kazuki Hashimoto <kazukih@tuta.io>
2022-03-24 08:27:31 +0900

    qcacld-3.0: Fix compilation
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -16806,7 +16806,7 @@ static void hdd_inform_wifi_on(void)
 }
 #endif
 
-static int hdd_driver_load(void);
+int hdd_driver_load(void);
 static ssize_t wlan_hdd_state_ctrl_param_write(struct file *filp,
 						const char __user *user_buf,
 						size_t count,
