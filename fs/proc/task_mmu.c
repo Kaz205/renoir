ce76926762afb081c5188a717f208240be351ce2
Suleiman Souhlal <suleiman@google.com>
2022-03-31 12:56:38 +0900

    CHROMIUM: mm: Check pmd_trans_unstable() after splitting huge page in per-process reclaim.
    Otherwise we may crash with NULL pointer dereference for shmem mappings,
    because splitting clears the PMD.
    
    Upstream commit fe932d4c9ea2 ("mm: validate pmd after splitting") fixes
    a similar issue.
    
    Fixes: 4c3ad28b9c91 ("CHROMIUM: mm: per-process reclaim")
    
    BUG=none
    TEST=echo shmem 1000 > /proc/`pgrep crosvm`/reclaim
    
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    Change-Id: I31fbac8add74f473d83d35ba54a2ad1d5737ee87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3562538
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Commit-Queue: Suleiman Souhlal <suleiman@chromium.org>
    Tested-by: Suleiman Souhlal <suleiman@chromium.org>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1906,10 +1906,10 @@ huge_unlock:
 		return 0;
 	}
 
+regular_page:
 	if (pmd_trans_unstable(pmd))
 		return 0;
 
-regular_page:
 	orig_pte = pte_offset_map_lock(vma->vm_mm, pmd, addr, &ptl);
 	for (pte = orig_pte; addr < end; pte++, addr += PAGE_SIZE) {
 		ptent = *pte;
@@ -2015,10 +2015,10 @@ huge_unlock:
 		return 0;
 	}
 
+regular_page:
 	if (pmd_trans_unstable(pmd))
 		return 0;
 
-regular_page:
 	orig_pte = pte_offset_map_lock(vma->vm_mm, pmd, addr, &ptl);
 	for (pte = orig_pte; addr < end; pte++, addr += PAGE_SIZE) {
 		ptent = *pte;
