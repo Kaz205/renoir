b8daa4919c83ffa4b2839623429e165b89842505
Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
2022-04-02 01:20:03 +0000

    UPSTREAM: media: venus: vdec: fixed possible memory leak issue
    The venus_helper_alloc_dpb_bufs() implementation allows an early return
    on an error path when checking the id from ida_alloc_min() which would
    not release the earlier buffer allocation.
    
    Move the direct kfree() from the error checking of dma_alloc_attrs() to
    the common fail path to ensure that allocations are released on all
    error paths in this function.
    
    Addresses-Coverity: 1494120 ("Resource leak")
    
    cc: stable@vger.kernel.org # 5.16+
    Fixes: 40d87aafee29 ("media: venus: vdec: decoded picture buffer handling during reconfig sequence")
    Signed-off-by: Ameer Hamza <amhamza.mgc@gmail.com>
    Reviewed-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Signed-off-by: Stanimir Varbanov <stanimir.varbanov@linaro.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    (cherry picked from commit 8403fdd775858a7bf04868d43daea0acbe49ddfc)
    
    BUG=b:190629171
    TEST=emerge-herobrine sys-kernel/chromeos-kernel-5_10
    
    Signed-off-by: Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
    Change-Id: Ic498e44075026dc26db83d07b60370d882d964e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3566743
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Tested-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>

--- drivers/media/platform/qcom/venus/helpers.c
+++ drivers/media/platform/qcom/venus/helpers.c
@@ -186,7 +186,6 @@ int venus_helper_alloc_dpb_bufs(struct venus_inst *inst)
 		buf->va = dma_alloc_attrs(dev, buf->size, &buf->da, GFP_KERNEL,
 					  buf->attrs);
 		if (!buf->va) {
-			kfree(buf);
 			ret = -ENOMEM;
 			goto fail;
 		}
@@ -206,6 +205,7 @@ int venus_helper_alloc_dpb_bufs(struct venus_inst *inst)
 	return 0;
 
 fail:
+	kfree(buf);
 	venus_helper_free_dpb_bufs(inst);
 	return ret;
 }
