e36e41c3a2d98966d3ca7d9eb379e47d271e6c11
Kazuki Hashimoto <kazukih@tuta.io>
2022-05-25 03:57:00 +0900

    Revert "ANDROID: mm: use alloc_flags for cma first alloc policy"
    This reverts commit b28864aa2ff01bfa1ef99ebbe82938fa4eb82674.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3312,7 +3312,7 @@ static struct page *__rmqueue_pcplist(struct zone *zone, int migratetype,
 	do {
 		/* First try to get CMA pages */
 		if (migratetype == MIGRATE_MOVABLE &&
-				alloc_flags & ALLOC_CMA) {
+				gfp_flags & __GFP_CMA) {
 			list = get_populated_pcp_list(zone, 0, pcp,
 					get_cma_migrate_type(), alloc_flags);
 		}
@@ -3394,7 +3394,7 @@ struct page *rmqueue(struct zone *preferred_zone,
 		}
 
 		if (!page && migratetype == MIGRATE_MOVABLE &&
-				alloc_flags & ALLOC_CMA)
+				gfp_flags & __GFP_CMA)
 			page = __rmqueue_cma(zone, order, migratetype,
 					     alloc_flags);
 
