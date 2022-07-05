01577415e94f96e1d39e70ba6f51b9ec0f8b1b41
Kazuki Hashimoto <kazukih@tuta.io>
2022-07-05 17:44:46 +0900

    Revert "sched: promote nodes out of CONFIG_SCHED_DEBUG"
    This reverts commit 1cf356bf223165a6b869e781179573a36aca6d10.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1550,7 +1550,15 @@ static inline void __set_task_cpu(struct task_struct *p, unsigned int cpu)
 #endif
 }
 
-#define const_debug __read_mostly
+/*
+ * Tunables that become constants when CONFIG_SCHED_DEBUG is off:
+ */
+#ifdef CONFIG_SCHED_DEBUG
+# include <linux/static_key.h>
+# define const_debug __read_mostly
+#else
+# define const_debug const
+#endif
 
 #define SCHED_FEAT(name, enabled)	\
 	__SCHED_FEAT_##name ,
