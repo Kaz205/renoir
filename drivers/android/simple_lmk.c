2ac89ff75a4ed61156cdd350654d3fe5d73c6cc3
Kazuki Hashimoto <kazukih@tuta.io>
2022-08-08 02:27:23 +0900

    simple_lmk: simple_lmk_reclaim_thread: Run with the highest priority
    Gotta go fast.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -269,11 +269,9 @@ static void scan_and_kill(void)
 
 static int simple_lmk_reclaim_thread(void *data)
 {
-	static const struct sched_param sched_max_rt_prio = {
-		.sched_priority = MAX_RT_PRIO - 1
-	};
+	static const struct sched_param sched_zero_prio;
 
-	sched_setscheduler_nocheck(current, SCHED_FIFO, &sched_max_rt_prio);
+	sched_setscheduler_nocheck(current, SCHED_FIFO, &sched_zero_prio);
 	set_freezable();
 
 	while (1) {
