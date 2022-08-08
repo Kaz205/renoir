dacb1117730408f39990ae52f5a31957e894c205
Ricardo Ribalda <ribalda@chromium.org>
2022-08-08 15:41:06 +0200

    FROMLIST: media: uvcvideo: Fix InterfaceProtocol for Quanta camera
    The device is using a different InterfaceProtocol than the one set in
    the original quirk.
    
    Fixes: 95f03d973478 ("media: uvcvideo: Limit power line control for Quanta cameras")
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>
    (am from https://patchwork.kernel.org/patch/12938811/)
    (also found at https://lore.kernel.org/r/20220808134106.70100-1-ribalda@chromium.org)
    
    BUG=b:233211200
    TEST=v4l2-compliance
    
    Change-Id: I7ddbd7f66b38a7a08e5277d526cc452fbe58a0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3816861
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Tested-by: Ricardo Ribalda <ribalda@chromium.org>
    Commit-Queue: Ricardo Ribalda <ribalda@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -2754,7 +2754,7 @@ static const struct usb_device_id uvc_ids[] = {
 	  .idProduct		= 0x4034,
 	  .bInterfaceClass	= USB_CLASS_VIDEO,
 	  .bInterfaceSubClass	= 1,
-	  .bInterfaceProtocol	= 0,
+	  .bInterfaceProtocol	= 1,
 	  .driver_info		= (kernel_ulong_t)&uvc_ctrl_power_line_limited },
 	/* LogiLink Wireless Webcam */
 	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
