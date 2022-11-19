ee98e4db767c274e60c892145fe377dadcaa2dc2
Kazuki Hashimoto <kazukih@tuta.io>
2022-11-20 05:56:35 +0900

    sched: Hardcode for asym capacity CPUs
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -467,10 +467,6 @@ static inline bool rt_task_fits_capacity(struct task_struct *p, int cpu)
 	unsigned int max_cap;
 	unsigned int cpu_cap;
 
-	/* Only heterogeneous systems can benefit from this check */
-	if (!static_branch_unlikely(&sched_asym_cpucapacity))
-		return true;
-
 	min_cap = uclamp_eff_value(p, UCLAMP_MIN);
 	max_cap = uclamp_eff_value(p, UCLAMP_MAX);
 
@@ -1759,16 +1755,9 @@ static int find_lowest_rq(struct task_struct *task)
 	 * If we're on asym system ensure we consider the different capacities
 	 * of the CPUs when searching for the lowest_mask.
 	 */
-	if (static_branch_unlikely(&sched_asym_cpucapacity)) {
-
-		ret = cpupri_find_fitness(&task_rq(task)->rd->cpupri,
-					  task, lowest_mask,
-					  rt_task_fits_capacity);
-	} else {
-
-		ret = cpupri_find(&task_rq(task)->rd->cpupri,
-				  task, lowest_mask);
-	}
+	ret = cpupri_find_fitness(&task_rq(task)->rd->cpupri,
+				  task, lowest_mask,
+				  rt_task_fits_capacity);
 
 	if (!ret)
 		return -1; /* No targets found */
