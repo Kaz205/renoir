36ca6bf197f7aba46dbb5c2d99d5422cdeae6f39
Keith Busch <kbusch@kernel.org>
2023-01-26 13:51:25 -0800

    dmapool: create/destroy cleanup
    Set the 'empty' bool directly from the result of the function that
    determines its value instead of adding additional logic.
    
    Link: https://lkml.kernel.org/r/20230126215125.4069751-13-kbusch@meta.com
    Fixes: 2d55c16c0c54 ("dmapool: create/destroy cleanup")
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Tony Battersby <tonyb@cybernetics.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/dmapool.c
+++ mm/dmapool.c
@@ -225,7 +225,7 @@ struct dma_pool *dma_pool_create(const char *name, struct device *dev,
 {
 	struct dma_pool *retval;
 	size_t allocation;
-	bool empty = false;
+	bool empty;
 
 	if (!dev)
 		return NULL;
@@ -275,8 +275,7 @@ struct dma_pool *dma_pool_create(const char *name, struct device *dev,
 	 */
 	mutex_lock(&pools_reg_lock);
 	mutex_lock(&pools_lock);
-	if (list_empty(&dev->dma_pools))
-		empty = true;
+	empty = list_empty(&dev->dma_pools);
 	list_add(&retval->pools, &dev->dma_pools);
 	mutex_unlock(&pools_lock);
 	if (empty) {
@@ -360,7 +359,7 @@ static struct dma_page *pool_alloc_page(struct dma_pool *pool, gfp_t mem_flags)
 void dma_pool_destroy(struct dma_pool *pool)
 {
 	struct dma_page *page, *tmp;
-	bool empty = false, busy = false;
+	bool empty, busy = false;
 
 	if (unlikely(!pool))
 		return;
@@ -368,8 +367,7 @@ void dma_pool_destroy(struct dma_pool *pool)
 	mutex_lock(&pools_reg_lock);
 	mutex_lock(&pools_lock);
 	list_del(&pool->pools);
-	if (list_empty(&pool->dev->dma_pools))
-		empty = true;
+	empty = list_empty(&pool->dev->dma_pools);
 	mutex_unlock(&pools_lock);
 	if (empty)
 		device_remove_file(pool->dev, &dev_attr_pools);
