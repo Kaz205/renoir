ee585f035822a814ce137f9b5b074e58217e914f
Kazuki H <kazukih0205@gmail.com>
2023-04-27 17:04:10 +0900

    build_kernel: Get hash of HEAD early
    I may add commits while it's linking.
    
    Signed-off-by: Kazuki H <kazukih0205@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,6 +3,7 @@ export SUBARCH=arm64
 export ak=$HOME/AnyKernel3
 export PATH="$HOME/llvm-16.0.2/bin:$PATH"
 
+NAME=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
 MAKE_PARAMS="LLVM=1 LLVM_IAS=1"
 git submodule init && git submodule update
 
@@ -15,7 +16,6 @@ then
     exit 1
 fi
 
-NAME=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
 cp out/arch/arm64/boot/Image $ak/Image
 find out/arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > $ak/dtb
 cp out/arch/arm64/boot/dtbo.img $ak/dtbo.img
