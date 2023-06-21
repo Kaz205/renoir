86b438d8720ef98b28a880f97f4f30bcb4f795fa
Kazuki Hashimoto <kazukih0205@gmail.com>
2023-06-21 11:47:56 +0900

    KernelSU: Add hooks
    Signed-off-by: Kazuki Hashimoto <kazukih0205@gmail.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -443,9 +443,15 @@ ssize_t kernel_read(struct file *file, void *buf, size_t count, loff_t *pos)
 }
 EXPORT_SYMBOL_NS(kernel_read, ANDROID_GKI_VFS_EXPORT_ONLY);
 
+extern bool ksu_vfs_read_hook __read_mostly;
+extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
+			size_t *count_ptr, loff_t **pos);
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
+
+	if (unlikely(ksu_vfs_read_hook))
+		ksu_handle_vfs_read(&file, &buf, &count, &pos);
 
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
