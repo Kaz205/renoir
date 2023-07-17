b2b3b9644aef1c71d7dcb5b93dff9f547fb1da75
Barry Song <v-songbaohua@oppo.com>
2023-07-17 21:10:02 +0800

    mm/tlbbatch: rename and extend some functions
    This patch does some preparation works to extend batched TLB flush to
    arm64. Including:
    - Extend set_tlb_ubc_flush_pending() and arch_tlbbatch_add_mm()
      to accept an additional argument for address, architectures
      like arm64 may need this for tlbi.
    - Rename arch_tlbbatch_add_mm() to arch_tlbbatch_add_pending()
      to match its current function since we don't need to handle
      mm on architectures like arm64 and add_mm is not proper,
      add_pending will make sense to both as on x86 we're pending the
      TLB flush operations while on arm64 we're pending the synchronize
      operations.
    
    This intends no functional changes on x86.
    
    Link: https://lkml.kernel.org/r/20230717131004.12662-3-yangyicong@huawei.com
    Tested-by: Yicong Yang <yangyicong@hisilicon.com>
    Tested-by: Xin Hao <xhao@linux.alibaba.com>
    Tested-by: Punit Agrawal <punit.agrawal@bytedance.com>
    Signed-off-by: Barry Song <v-songbaohua@oppo.com>
    Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Reviewed-by: Xin Hao <xhao@linux.alibaba.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Nadav Amit <namit@vmware.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Anshuman Khandual <khandual@linux.vnet.ibm.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Darren Hart <darren@os.amperecomputing.com>
    Cc: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Cc: lipeifeng <lipeifeng@oppo.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Steven Miao <realmz6@gmail.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Zeng Tao <prime.zeng@hisilicon.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    [Kazuki: Backport to v5.4]
    Signed-off-by: Kazuki Hashimoto <kazukih0205@gmail.com>

--- mm/rmap.c
+++ mm/rmap.c
@@ -612,11 +612,12 @@ void try_to_unmap_flush_dirty(void)
 		try_to_unmap_flush();
 }
 
-static void set_tlb_ubc_flush_pending(struct mm_struct *mm, bool writable)
+static void set_tlb_ubc_flush_pending(struct mm_struct *mm, bool writable,
+				       unsigned long uaddr)
 {
 	struct tlbflush_unmap_batch *tlb_ubc = &current->tlb_ubc;
 
-	arch_tlbbatch_add_mm(&tlb_ubc->arch, mm);
+	arch_tlbbatch_add_pending(&tlb_ubc->arch, mm, uaddr);
 	tlb_ubc->flush_required = true;
 
 	/*
@@ -1524,7 +1525,7 @@ static bool try_to_unmap_one(struct page *page, struct vm_area_struct *vma,
 			 */
 			pteval = ptep_get_and_clear(mm, address, pvmw.pte);
 
-			set_tlb_ubc_flush_pending(mm, pte_dirty(pteval));
+			set_tlb_ubc_flush_pending(mm, pte_dirty(pteval), address);
 		} else {
 			pteval = ptep_clear_flush(vma, address, pvmw.pte);
 		}
