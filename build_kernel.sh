22d37f5618bee4264f837a1e40b823b4e3dc0b07
Kazuki H <kazukih0205@gmail.com>
2023-10-20 01:12:37 +0900

    build_kernel: Switch to LLVM 17.0.3

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 export ARCH=arm64
 export SUBARCH=arm64
 export ak=$HOME/AnyKernel3
-export PATH="$HOME/llvm-17.0.1/bin:$PATH"
+export PATH="$HOME/llvm-17.0.3/bin:$PATH"
 
 NAME=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
 MAKE_PARAMS="LLVM=1 LLVM_IAS=1"
